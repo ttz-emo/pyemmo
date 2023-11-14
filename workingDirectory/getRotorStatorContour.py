cab899928406bc8cd9409eda58e73d76d6a981b6
Jan Knoll <jan.knoll@study.thws.de>
2023-11-14 16:54:11 +0100

    [BF] Fixed the bug, where some lines where removed from the geometryList, so the rotor wasn't complete.

--- workingDirectory/getRotorStatorContour.py
+++ workingDirectory/getRotorStatorContour.py
@@ -13,7 +13,8 @@ from workingDirectory.getRotorStatorSurfaces import getRotorSurfaces, getStatorS
 from pyemmo.functions.plot import plot
 from pyemmo.script.geometry.circleArc import CircleArc
 from pyemmo.script.geometry.point import Point
- 
+
+
 def getRotorContour(geometryList, machine):
     """Get a line list of the rotor-contour. ``rotorContourLineList``
 
@@ -24,121 +25,89 @@ def getRotorContour(geometryList, machine):
     Returns:
         _type_: _description_
     """
-    rotorContourLineList = []
-
-    rotorLamSurfList, rotorMagSurfList = getRotorSurfaces(geometryList=geometryList)
-
-    # =================================================
-    # Erstellung der Rotor-Konturlinie fuer MovingBand:
-    # =================================================
-
-    rotorLamSurfTestList = rotorLamSurfList.copy()
-    rotorMagSurfTestList = rotorMagSurfList.copy()
-
-    # -----------------------------------------------------------
-    # Aussortieren der Linien die nicht an der Oberfläche liegen:
-    # -----------------------------------------------------------
-    a = 0
     rotorRint = machine.rotor.Rint
-    while a < len(rotorLamSurfTestList[0].curve):
-        if math.isclose(
-            a=rotorLamSurfTestList[0].curve[a].startPoint.radius,
-            b=rotorRint,
-            abs_tol=1e-6,
-        ) or math.isclose(
-            a=rotorLamSurfTestList[0].curve[a].endPoint.radius,
-            b=rotorRint,
-            abs_tol=1e-6,
-        ):
-            rotorLamSurfTestList[0].curve.remove(rotorLamSurfTestList[0].curve[a])
-        else:
-            a += 1
-
-    rotorContourLineList.extend(rotorLamSurfList[0].curve)
-    rotorContourLineList.extend(rotorMagSurfList[0].curve)
-    plot(rotorContourLineList, linewidth=1, markersize=3)
-    print("---")
-    print("Plot Überprüfung des Löschens der Seitenlinien.")
-    print("---")
-
     rotorRext = machine.rotor.Rext
     H0 = machine.rotor.slot.H0
     Hmag = machine.rotor.slot.Hmag
     magnetFarthestRadius = rotorRext + Hmag - H0
 
+    rotorContourLineList = []
+    rotorSlotBottomLineList = []
+    rotorLamSurfList, rotorMagSurfList = getRotorSurfaces(geometryList=geometryList)
+
+    # =================================================
+    # Erstellung der Rotor-Konturlinie fuer MovingBand:
+    # =================================================
+    # -----------------------------------------------------------
+    # Aussortieren der Linien die nicht an der Oberfläche liegen:
+    # -----------------------------------------------------------
+    # rotor lamination:
+    for a, curve in enumerate(rotorLamSurfList[0].curve):
+        if (
+            math.isclose(a=curve.startPoint.radius, b=rotorRint, abs_tol=1e-6) is False
+            and math.isclose(a=curve.endPoint.radius, b=rotorRint, abs_tol=1e-6)
+            is False
+        ):
+            rotorContourLineList.append(curve)
+
+    print("---")
+    print("Plot Überprüfung des Löschens der Seitenlinien.")
+    plot(rotorContourLineList, linewidth=1, markersize=3)
+    print("---")
+
     if machine.rotor.slot.H0 != 0:
         # -----------------------------
         # Split von Linien/Kreisboegen:
         # -----------------------------
-        xx = 0
-        while xx < len(rotorLamSurfTestList[0].curve):
-            if math.isclose(  # If startPointRadius == Rext:
-                a=rotorLamSurfTestList[0].curve[xx].startPoint.radius,
-                b=rotorRext,
-                abs_tol=1e-6,
-            ) or math.isclose(  # If endPointRadius == Rext:
-                a=rotorLamSurfTestList[0].curve[xx].endPoint.radius,
-                b=rotorRext,
-                abs_tol=1e-6,
+        # Heraussuchen der Boeden von Magnet und Slot
+        for a, curve in enumerate(rotorContourLineList):
+            if (
+                math.isclose(a=curve.startPoint.radius, b=rotorRext, abs_tol=1e-6)
+                is False
+                and math.isclose(a=curve.endPoint.radius, b=rotorRext, abs_tol=1e-6)
+                is False
             ):
-                rotorLamSurfTestList[0].curve.remove(rotorLamSurfTestList[0].curve[xx])
-            else:
-                for yy, rotorMagSurf in enumerate(rotorMagSurfTestList):
-                    zz = 0
-                    while zz < len(rotorMagSurf.curve):
-                        if (
-                            math.isclose(
-                                a=rotorMagSurf.curve[zz].startPoint.radius,
-                                b=rotorRext,
-                                abs_tol=1e-6,
-                            )
-                            or math.isclose(
-                                a=rotorMagSurf.curve[zz].endPoint.radius,
-                                b=rotorRext,
-                                abs_tol=1e-6,
-                            )
-                            or math.isclose(  # If startPointRadius == magnetFartestRadius
-                                a=rotorMagSurf.curve[zz].startPoint.radius,
-                                b=magnetFarthestRadius,
-                                abs_tol=1e-6,
-                            )
-                            or math.isclose(  # If endPointRadius == magnetFartestRadius
-                                a=rotorMagSurf.curve[zz].endPoint.radius,
-                                b=magnetFarthestRadius,
-                                abs_tol=1e-6,
-                            )
-                        ):
-                            rotorMagSurf.curve.remove(rotorMagSurf.curve[zz])
+                rotorSlotBottomLineList.append(curve)
 
-                        else:
-                            zz += 1
-                    xx += 1
+        for a, magnet in enumerate(rotorMagSurfList):
+            for b, curve in enumerate(magnet.curve):
+                rotorContourLineList.append(curve)
+                if (
+                    math.isclose(
+                        a=curve.startPoint.radius, b=magnetFarthestRadius, abs_tol=1e-6
+                    )
+                    is False
+                    and math.isclose(
+                        a=curve.endPoint.radius, b=magnetFarthestRadius, abs_tol=1e-6
+                    )
+                    is False
+                ):
+                    rotorSlotBottomLineList.append(curve)
 
         # ----------------------------------------------
         # Plots des Magnettaschenbodens und Magnetboden:
         # ----------------------------------------------
 
         print("Plot Rotornutboden und Magnetboden.")
-        plot(rotorLamSurfTestList[0].curve, linewidth=1, markersize=3, tag=True)
-        plot(rotorMagSurfTestList[0].curve, linewidth=1, markersize=3, tag=True)
+        plot(rotorSlotBottomLineList, linewidth=1, markersize=3, tag=True)
         print("---")
 
         # ------------------------------------------------
         # Loeschen des Rotornutboden und Magnetboden:
         # ------------------------------------------------
-
-        rotorContourLineList.remove(rotorLamSurfTestList[0].curve[0])
-        rotorContourLineList.remove(rotorMagSurfTestList[0].curve[0])
+        for a, curve in enumerate(rotorSlotBottomLineList):
+            rotorContourLineList.remove(curve)
+        # rotorContourLineList.remove(rotorSlotBottomLineList[0])
 
         # ---------------------------------------------------
         # createMagGrndCurve(): Erzeugung der 3 neuen Curves:
         # ---------------------------------------------------
         # Points:
-        centerPointNewCircles = rotorMagSurfTestList[0].curve[0].center
-        startPointNewCircle1 = rotorLamSurfTestList[0].curve[0].startPoint
-        endPointNewCircle1 = rotorMagSurfTestList[0].curve[0].endPoint
-        startPointNewCircle2 = rotorMagSurfTestList[0].curve[0].startPoint
-        endPointNewCircle2 = rotorLamSurfTestList[0].curve[0].endPoint
+        centerPointNewCircles = Point(name="centerPoint", x=0, y=0, z=0, meshLength=1)
+        startPointNewCircle1 = rotorSlotBottomLineList[0].startPoint
+        endPointNewCircle1 = rotorSlotBottomLineList[1].endPoint
+        startPointNewCircle2 = rotorSlotBottomLineList[1].startPoint
+        endPointNewCircle2 = rotorSlotBottomLineList[0].endPoint
 
         # Erstellung von zwei neuen Curves:
         newCurve1 = CircleArc(
@@ -182,6 +151,7 @@ def getRotorContour(geometryList, machine):
     plot(rotorContourLineList, linewidth=1, markersize=3, tag=True)
     print("---")
     return rotorContourLineList
+
 
 def getStatorContour(geometryList, machine):
     statorContourLineList = []
