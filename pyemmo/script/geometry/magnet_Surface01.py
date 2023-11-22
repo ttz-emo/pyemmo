346bf39aa5a5569adf75d1a42fb24eede5315bcd
Max Schuler <max.schuler@thws.de>
2023-11-22 16:26:23 +0100

    [BF] added missing mag vec angle default setting

--- pyemmo/script/geometry/magnet_Surface01.py
+++ pyemmo/script/geometry/magnet_Surface01.py
@@ -1,12 +1,10 @@
 from .magnet import Magnet
-from .physicalElement import PhysicalElement
 from .point import Point
 from .line import Line
 from .circleArc import CircleArc
 from .surface import Surface
 from .. import colorDict
 
-import math
 
 ###
 # Ein Objekt der Klasse Magnet_Surface01:
@@ -39,12 +37,12 @@ class Magnet_Surface01(Magnet):
         h_M (float): Magnet hight
         angularWidth_i (float): Inner angular width
         angularWidth_a (float): Outer angular width
-        magnetisationDirection (Literial[-1,1]): see definition in Class 
+        magnetisationDirection (Literial[-1,1]): see definition in Class
             :class:`~pyemmo.script.geometry.magnet.Magnet`
-        magnetisationType (str): see definition in Class 
+        magnetisationType (str): see definition in Class
             :class:`~pyemmo.script.geometry.magnet.Magnet`
         material (Material): Material of magnet.
-        meshLength (float): 
+        meshLength (float):
 
     .. figure:: ../image_Doku/magnet_Surface01.png
         :align: center
@@ -74,10 +72,10 @@ class Magnet_Surface01(Magnet):
             machineDict["material"],
             magnetisationDirection,
             magnetisationType,
+            0.0,
         )
 
         self._createGeometry()
-
 
     ###_createGeometry() erzeugt die Magnetgeometrie und definiert alle Attribute der Klasse.
     def _createGeometry(self):
@@ -194,4 +192,3 @@ class Magnet_Surface01(Magnet):
             list: _laminationDockingPoint
         """
         return self._laminationDockingPoint
-    
