557c89851c405f71b35977c3c6a948e61f25aede
Jan Knoll <jan.knoll@study.thws.de>
2023-12-12 13:56:16 +0100

    [WP] added a new translation for testing

--- workingDirectory/translateWinding.py
+++ workingDirectory/translateWinding.py
@@ -26,9 +26,43 @@ def translateWinding(
         Q=machine.stator.slot.Zs,
         P=machine.stator.winding.p * 2,
         m=machine.stator.winding.qs,
+        w=machine.stator.winding.coil_pitch,
         layers=machine.stator.winding.Nlayer,
         turns=machine.stator.winding.Ntcoil,
     )
     windSwat = winding.get_phases()
-
+    windTest = machine.stator.winding.wind_mat
+    windTestSwat = [[[], []], [[], []], [[], []]]
+    isDblLayer = bool(len(windTest) > 1)
+    if isDblLayer:
+        for a in windTest:
+            for slot, conductor in enumerate(a[0]):
+                if conductor[0] > 0:
+                    windTestSwat[0][0].append(slot + 1)
+                elif conductor[0] < 0:
+                    windTestSwat[0][1].append(-(slot + 1))
+                elif conductor[1] > 0:
+                    windTestSwat[1][0].append(slot + 1)
+                elif conductor[1] < 0:
+                    windTestSwat[1][1].append(-(slot + 1))
+                elif conductor[2] > 0:
+                    windTestSwat[2][0].append(slot + 1)
+                elif conductor[2] < 0:
+                    windTestSwat[2][1].append(-(slot + 1))
+    else:
+        for a in windTest:
+            for slot, conductor in enumerate(a[0]):
+                if conductor[0] > 0:
+                    windTestSwat[0][0].append(slot + 1)
+                elif conductor[0] < 0:
+                    windTestSwat[0][0].append(-(slot + 1))
+                elif conductor[1] > 0:
+                    windTestSwat[1][0].append(slot + 1)
+                elif conductor[1] < 0:
+                    windTestSwat[1][0].append(-(slot + 1))
+                elif conductor[2] > 0:
+                    windTestSwat[2][0].append(slot + 1)
+                elif conductor[2] < 0:
+                    windTestSwat[2][0].append(-(slot + 1))
+            
     return windSwat
