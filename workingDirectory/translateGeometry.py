fa6d9586bd42228f61d5bd40324aa0ecdeb4e0f9
Max Schuler <max.schuler@thws.de>
2023-12-12 22:49:42 +0100

    [WP] changed slod idExt in case R1 and T0 from StCu0 to StCu1 [CC] added TODO

--- workingDirectory/translateGeometry.py
+++ workingDirectory/translateGeometry.py
@@ -22,7 +22,6 @@ def translateGeometry(
     surface: pyleecan.Classes.Surface.Surface,
     anglePointRefList: list,
 ) -> tuple[SurfaceAPI, list]:
-    
     isMag = False
     if nameSplitList[0] == "Rotor":
         if nameSplitList[2] == "Lamination":
@@ -46,7 +45,7 @@ def translateGeometry(
 
         elif nameSplitList[2] == "HoleMag":
             pyleecanMat = machine.rotor.hole[0].magnet_0.mat_type
-            
+
             idExt = "Mag"  # "Magnet"
             name = "Magnet"
             anglePointRefList.append(angle(surface.point_ref))
@@ -74,7 +73,6 @@ def translateGeometry(
             meshSize=0,
         )
 
-
     # stator
     elif nameSplitList[0] == "Stator":
         if nameSplitList[2] == "Lamination":
@@ -95,19 +93,25 @@ def translateGeometry(
                         idExt = "StCu0"  # "Stator-Nut"
                     else:
                         idExt = "StCu1"  # "Stator-Nut"
-                else: 
+                else:
                     idExt = "StCu0"  # "Stator-Nut"
-            else: # nameSplitList[3] == "R1":
-                # BUG, FIXME: 'T0' kann für q=0.5 Wicklung mit Ober- und 
+            elif nameSplitList[3] == "R1":
+                # BUG, FIXME: 'T0' kann für q=0.5 Wicklung mit Ober- und
                 # Schicht vorkommen. Dann ist für R0 und R1 beides mal 'T0'
                 # Das führt dazu, dass beide StCu0 benannt werden und nur eins
                 # der beiden Segmente erzeugt wird!
                 if q == 0.5 and nameSplitList[4] == "T0":
-                    idExt = "StCu0"  # "Stator-Nut"
+                    idExt = "StCu1"  # "Stator-Nut"
                 elif q == 0.5 and nameSplitList[4] == "T1":
+                    # TODO: Außen liegende und linke Nuthälfte... Geht das? Mehr als 
+                    # zwei Wicklungen pro Nut?
                     idExt = "StCu1"  # "Stator-Nut"
-                else: 
+                else:
                     idExt = "StCu1"  # "Stator-Nut"
+            else:
+                raise ValueError(
+                    f"Radial Index of '{nameSplitList.join('_')}' was not R0 or R1, but '{nameSplitList[3]}'"
+                )
 
         else:
             raise ValueError(
