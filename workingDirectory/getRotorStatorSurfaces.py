133f8448c9ebf8252a835a6f15ad523f67c5b3a8
Jan Knoll <jan.knoll@study.thws.de>
2023-12-14 17:16:43 +0100

    [CC] added imports, added typehints, added docstring and changed print() to logging.debug()

--- workingDirectory/getRotorStatorSurfaces.py
+++ workingDirectory/getRotorStatorSurfaces.py
@@ -1,56 +1,66 @@
-def getRotorSurfaces(geometryList):
-    """Filter the rotor-surfaces in ``lamination`` and ``magnet`` via surface name.
+import logging
+from pyemmo.api.SurfaceJSON import SurfaceAPI
+
+
+def getRotorSurfaces(
+    geometryList: list[SurfaceAPI],
+) -> tuple[list[SurfaceAPI], list[SurfaceAPI]]:
+    """Get the surfaces of the rotor.
 
     Args:
-        geometryList (_type_): _description_
+        geometryList (list[SurfaceAPI]): list with all surfaces of the machine in pyemmo format
 
     Returns:
-        _type_: _description_
+        tuple[list[SurfaceAPI], list[SurfaceAPI]]: _description_
     """
     # =========================================
     # Zuweisung der Surfaces zu den Kategorien:
     # =========================================
     rotorLamSurfList = []
     rotorMagSurfList = []
-    
+
     for surf in geometryList:
         if surf.idExt == "Pol":
             rotorLamSurfList.append(surf)
-            print("rotorLamSurf:")
-            print(f"gefunden: {surf.name}")
+            logging.debug("rotorLamSurf:")
+            logging.debug(f"gefunden: {surf.name}")
         elif surf.idExt == "Mag0":
             rotorMagSurfList.append(surf)
-            print("rotorMagSurf:")
-            print(f"gefunden: {surf.name}")
+            logging.debug("rotorMagSurf:")
+            logging.debug(f"gefunden: {surf.name}")
         elif surf.idExt == "Mag1":
             rotorMagSurfList.append(surf)
-            print("rotorMagSurf:")
-            print(f"gefunden: {surf.name}")
+            logging.debug("rotorMagSurf:")
+            logging.debug(f"gefunden: {surf.name}")
         elif surf.idExt == "Mag2":
             rotorMagSurfList.append(surf)
-            print("rotorMagSurf:")
-            print(f"gefunden: {surf.name}")
+            logging.debug("rotorMagSurf:")
+            logging.debug(f"gefunden: {surf.name}")
+
     return rotorLamSurfList, rotorMagSurfList
 
-def getStatorSurfaces(geometryList):
-    """Filter the stator-surfaces in ``lamination`` and ``winding`` via surface name.
+
+def getStatorSurfaces(
+    geometryList: list[SurfaceAPI],
+) -> list[SurfaceAPI]:
+    """Get the surface of the stator lamination.
 
     Args:
-        geometryList (_type_): _description_
+        geometryList (list[SurfaceAPI]): list with all surfaces of the machine in pyemmo format
 
     Returns:
-        _type_: _description_
+        list[SurfaceAPI]: _description_
     """
     # =========================================
     # Zuweisung der Surfaces zu den Kategorien:
     # =========================================
     statorLamSurfList = []
     statorWindSurfList = []
-    
+
     for surf in geometryList:
         if surf.idExt == "StNut":
             statorLamSurfList.append(surf)
-            print("statorLamSurf:")
-            print(f"gefunden: {surf.name}")
+            logging.debug("statorLamSurf:")
+            logging.debug(f"gefunden: {surf.name}")
 
     return statorLamSurfList
