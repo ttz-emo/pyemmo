d3b09edfa7787831a0d54e70ea2d80b825d1aba8
Jan Knoll <jan.knoll@study.thws.de>
2024-01-08 10:47:57 +0100

    [CC] added docstrings and imports

--- workingDirectory/translateArc1.py
+++ workingDirectory/translateArc1.py
@@ -1,18 +1,24 @@
+"""imports"""
 import pyleecan.Classes.Arc1
 
+from pyemmo.script.geometry.point import Point
 from .buildPyemmoPoint import buildPyemmoPoint
+
 
 # =======================================
 # Definition of function 'translateArc1':
 # =======================================
-def translateArc1(line: pyleecan.Classes.Arc1.Arc1):
-    """_summary_
+def translateArc1(
+    line: pyleecan.Classes.Arc1.Arc1,
+) -> tuple[Point, Point, Point, Point]:
+    """Translates a pyleecan Arc1 into a pyemmo CircleArc.
 
     Args:
-        line (pyleecan.Classes.Arc1.Arc1): _description_
+        line (pyleecan.Classes.Arc1.Arc1): a pyleecan Arc1 object
 
     Returns:
-        _type_: _description_
+        tuple[Point, Point, Point, Point]: startPoint, endPoint, center point of the arc
+        and the middle point on the arc
     """
     # Translation for Pyleecan Arc1 to Pyemmo CircleArc
     # (An arc between two points (defined by a radius)):
@@ -22,4 +28,7 @@ def translateArc1(line: pyleecan.Classes.Arc1.Arc1):
     centerPoint = buildPyemmoPoint(line.get_center())
     centerPointArc = buildPyemmoPoint(line.get_middle())
 
+    
+    
+    
     return startPoint, endPoint, centerPoint, centerPointArc
--- workingDirectory/translateArc2.py
+++ workingDirectory/translateArc2.py
@@ -1,26 +1,20 @@
-import math
-import sys
-
-try:
-    from pyemmo.script.script import Script
-except:
-    rootname = "C:\\Users\\k49976\\Desktop\\repositoryGibLab\\pyemmo"
-    print(f"Could not determine root. Setting it manually to '{rootname}'")
-    print(f'rootname is "{rootname}"')
-    sys.path.append(rootname)
-
-from .buildPyemmoPoint import buildPyemmoPoint
-from pyemmo.script.geometry.point import Point
+"""imports"""
 import pyleecan.Classes.Arc2
+
+from pyemmo.script.geometry.point import Point
+from .buildPyemmoPoint import buildPyemmoPoint
+
 
 # =======================================
 # Definition of function 'translateArc2':
 # =======================================
-def translateArc2(line: pyleecan.Classes.Arc2.Arc2):
-    """_summary_
+def translateArc2(
+    line: pyleecan.Classes.Arc2.Arc2,
+) -> tuple[Point, Point, Point]:
+    """Translates a pyleecan Arc2 into a pyemmo CircleArc.
 
     Args:
-        line (pyleecan.Classes.Arc2.Arc2): _description_
+        line (pyleecan.Classes.Arc2.Arc2): a pyleecan Arc2 object
 
     Returns:
         _type_: _description_
@@ -30,20 +24,7 @@ def translateArc2(line: pyleecan.Classes.Arc2.Arc2):
 
     startPoint = buildPyemmoPoint(line.begin)
     centerPoint = buildPyemmoPoint(line.center)
-    # FIXME: namen von centerpoint in der klammer überprüfen, ob er mit der von pyleecan übereinstimmt
+    middlePoint = buildPyemmoPoint(line.get_middle())
+    endPoint = buildPyemmoPoint(line.get_end())
 
-    # Berechnung des Endpunkts des Kreisbogens
-    angleRad = line.get_angle()
-    endPoint_x = (
-        centerPoint.coordinate[0]
-        + (startPoint.coordinate[0] - centerPoint.coordinate[0]) * math.cos(angleRad)
-        - (startPoint.coordinate[1] - centerPoint.coordinate[1]) * math.sin(angleRad)
-    )
-    endPoint_y = (
-        centerPoint.coordinate[1]
-        + (startPoint.coordinate[0] - centerPoint.coordinate[0]) * math.sin(angleRad)
-        + (startPoint.coordinate[1] - centerPoint.coordinate[1]) * math.cos(angleRad)
-    )
-    endPoint = Point(name="endPoint", x=endPoint_x, y=endPoint_y, z=0, meshLength=1.0)
-
-    return startPoint, endPoint, centerPoint
+    return startPoint, endPoint, centerPoint, middlePoint
