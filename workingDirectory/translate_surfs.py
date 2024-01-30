31531e2e76b946dec6cceb0a47d94f3b385d71e2
Jan Knoll <jan.knoll@study.thws.de>
2024-01-30 08:56:35 +0100

    [CO] optimized the detection of the surface name [CC] added module docstring and function docstring

--- workingDirectory/translate_surfs.py
+++ workingDirectory/translate_surfs.py
@@ -1,3 +1,28 @@
+"""
+Module: translate_surfs
+
+This module provides functions for translating surfaces from pyleecan format to pyemmo format.
+
+Classes:
+    - SurfaceAPI: Represents a surface in pyemmo format.
+
+Functions:
+    - build_pyemmo_material(pyleecan_material: pyleecan.Classes.Material) -> Material:
+        Translates a pyleecan-material into a pyemmo-material.
+
+    - build_pyemmo_line_list(pyleecan_line_list: list) -> list:
+        Translates a list of pyleecan curves into a list of pyemmo curves.
+
+    - translate_surfs(
+        name_split_list: list[str],
+        machine: pyleecan.Classes.Machine.Machine,
+        surface: pyleecan.Classes.Surface.Surface,
+        angle_point_ref_list: list
+    ) -> tuple[SurfaceAPI, list]:
+        Translates pyleecan surfaces into pyemmo surfaces.
+
+"""
+
 from math import pi
 from numpy import angle
 
@@ -12,29 +37,26 @@ from .build_pyemmo_material import build_pyemmo_material
 from .build_pyemmo_line_list import build_pyemmo_line_list
 
 
-# ===========================================
-# Definition of function 'translateGeometry':
-# ===========================================
-def translate_surfs(
+def translate_surface(
     name_split_list: list[str],  # list with the splitted names
     machine: pyleecan.Classes.Machine.Machine,  # Import of motor
     surface: pyleecan.Classes.Surface.Surface,
     angle_point_ref_list: list,
 ) -> tuple[SurfaceAPI, list]:
-    """_summary_
+    """
+    Translates pyleecan surfaces into pyemmo surfaces.
 
     Args:
-        name_split_list (list[str]): _description_
-        angle_point_ref_list (list): _description_
+        name_split_list (list[str]): List with the splitted names.
+        machine (pyleecan.Classes.Machine.Machine): Import of the motor.
+        surface (pyleecan.Classes.Surface.Surface): Pyleecan surface to be translated.
+        angle_point_ref_list (list): List of angle point references.
 
     Raises:
-        ValueError: _description_
-        ValueError: _description_
-        ValueError: _description_
-        ValueError: _description_
+        ValueError: If there is an issue with the input values.
 
     Returns:
-        tuple[SurfaceAPI, list]: _description_
+        tuple[SurfaceAPI, list]: Translated pyemmo surface and updated angle point reference list.
     """
     if name_split_list[0] == "Rotor":
         if name_split_list[2] == "Lamination":
@@ -42,34 +64,29 @@ def translate_surfs(
             id_ext = "Pol"  # "RoNut" "Rotorblech"
             name = "Rotorblech"
 
-        elif name_split_list[2] == "Magnet":
-            pyleecan_mat = machine.rotor.magnet.mat_type
+        elif name_split_list[2] in ("Magnet", "Hole", "HoleMag"):
             id_ext = "Mag"
             name = "Magnet"
             angle_point_ref_list.append(angle(surface.point_ref))
 
-        elif name_split_list[2] == "Hole":
-            pyleecan_mat = machine.rotor.hole.mat_type
-            id_ext = "Mag"
-            name = "Magnet"
-            angle_point_ref_list.append(angle(surface.point_ref))
+            if name_split_list[2] == "Magnet":
+                pyleecan_mat = machine.rotor.magnet.mat_type
 
-        elif name_split_list[2] == "HoleMag":
-            pyleecan_mat = machine.rotor.hole[0].magnet_0.mat_type
+            elif name_split_list[2] == "Hole":
+                pyleecan_mat = machine.rotor.hole.mat_type
 
-            id_ext = "Mag"  # "Magnet"
-            name = "Magnet"
-            angle_point_ref_list.append(angle(surface.point_ref))
+            elif name_split_list[2] == "HoleMag":
+                pyleecan_mat = machine.rotor.hole[0].magnet_0.mat_type
 
-        elif name_split_list[2] == "HoleVoid":
-            pyleecan_mat = machine.rotor.hole[0].mat_void
+        elif name_split_list[2] in ("HoleVoid", "Ventilation"):
             id_ext = "Lpl"  # "Loch (Pollueke)"
             name = "Loch"
-
-        elif name_split_list[2] == "Ventilation":
-            pyleecan_mat = machine.rotor.axial_vent[0].mat_void
-            id_ext = "Lpl"  # "Loch (Pollueke)"
-            name = "Loch"
+            
+            if name_split_list[2] == "HoleVoid":
+                pyleecan_mat = machine.rotor.hole[0].mat_void
+            
+            elif name_split_list[2] == "Ventilation":
+                pyleecan_mat = machine.rotor.axial_vent[0].mat_void
 
         else:
             raise ValueError(
@@ -131,12 +148,11 @@ def translate_surfs(
         )
 
     angle_seg = 2 * pi / nbr_seg
-    pyemmo_mat = build_pyemmo_material(pyleecan_mat)
     pyemmo_surf = SurfaceAPI(
         name=name,
         idExt=id_ext,
         curves=build_pyemmo_line_list(surface.line_list),
-        material=pyemmo_mat,
+        material=build_pyemmo_material(pyleecan_mat),
         nbrSegments=nbr_seg,
         angle=angle_seg,
         meshSize=0,
