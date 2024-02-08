c387f3c3c5b36a4e4a9812f3ccefab49909c26da
Jan Knoll <jan.knoll@study.thws.de>
2024-02-08 13:20:53 +0100

    [NF] added full testing

--- tests/api/pyleecan/test_translate_winding.py
+++ tests/api/pyleecan/test_translate_winding.py
@@ -1,37 +1,55 @@
 import os
-import sys
+import pytest
 from pyleecan.Classes.Machine import Machine
 from pyleecan.Functions.load import load
 
-try:
-    from pyemmo.script.script import Script
-except:
-    rootname = "C:\\Users\\k49976\\Desktop\\repositoryGibLab\\pyemmo"
-    print(f"Could not determine root. Setting it manually to '{rootname}'")
-    print(f'rootname is "{rootname}"')
-    sys.path.append(rootname)
+from pyemmo.api.pyleecan.translate_winding import translate_winding
 from pyemmo.definitions import TEST_DIR
 
 
-def test_translate_winding_single_layer():
+@pytest.mark.parametrize(
+    "machine_sample",
+    [
+        "03_synrm_muster_Bachelor.json",
+        "03_synrm_muster_Bachelor_zaw.json",
+        "03_synrm_muster_Bachelor_zww.json",
+    ],
+)
+def test_translate_winding(machine_sample):
     machine: Machine = load(
-        os.path.abspath(
-            os.path.join(TEST_DIR, "data", "03_synrm_muster_Bachelor.json")
-        )
+        os.path.abspath(os.path.join(TEST_DIR, "data", machine_sample))
     )
+    _, wind_swat = translate_winding(machine)
 
+    if machine_sample == "03_synrm_muster_Bachelor.json":
+        assert wind_swat[0] == [[1, 2, -7, -8, 13, 14, -19, -20], []]
+        assert wind_swat[1] == [[5, 6, -11, -12, 17, 18, -23, -24], []]
+        assert wind_swat[2] == [[-3, -4, 9, 10, -15, -16, 21, 22], []]
 
-def test_translate_winding_tooth_coil():
-    machine: Machine = load(
-        os.path.abspath(
-            os.path.join(TEST_DIR, "data", "03_synrm_muster_Bachelor_zaw.json")
-        )
-    )
+    elif machine_sample == "03_synrm_muster_Bachelor_zaw.json":
+        assert wind_swat[0] == [
+            [1, 2, -7, -8, 13, 14, -19, -20],
+            [-7, -8, 13, 14, -19, -20, 1, 2],
+        ]
+        assert wind_swat[1] == [
+            [5, 6, -11, -12, 17, 18, -23, -24],
+            [-11, -12, 17, 18, -23, -24, 5, 6],
+        ]
+        assert wind_swat[2] == [
+            [-3, -4, 9, 10, -15, -16, 21, 22],
+            [9, 10, -15, -16, 21, 22, -3, -4],
+        ]
 
-
-def test_translate_winding_two_layer():
-    machine: Machine = load(
-        os.path.abspath(
-            os.path.join(TEST_DIR, "data", "03_synrm_muster_Bachelor_zww.json")
-        )
-    )
+    elif machine_sample == "03_synrm_muster_Bachelor_zww.json":
+        assert wind_swat[0] == [
+            [1, 2, -7, -8, 13, 14, -19, -20],
+            [-5, -6, 11, 12, -17, -18, 23, 24],
+        ]
+        assert wind_swat[1] == [
+            [5, 6, -11, -12, 17, 18, -23, -24],
+            [-9, -10, 15, 16, -21, -22, 3, 4],
+        ]
+        assert wind_swat[2] == [
+            [-3, -4, 9, 10, -15, -16, 21, 22],
+            [7, 8, -13, -14, 19, 20, -1, -2],
+        ]
