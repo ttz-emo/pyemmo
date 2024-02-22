2750906502ebbd9a45fd3540a2286bad195e3149
Max Schuler <max.schuler@thws.de>
2024-02-22 13:05:13 +0100

    [WP] moved contents of json command line interface to __main__.py module

--- pyemmo/api/json/json.py
+++ pyemmo/api/json/json.py
@@ -1,24 +1,23 @@
 """This is the main module of the json api to create a machine model in Onelab."""
+
 # import debugpy
 # debugpy.debug_this_thread()
 import os
 import subprocess
-import argparse
 from os import mkdir
 from os.path import isdir, isfile, join
 from typing import Dict, List, Tuple
 import logging
 import json
 import datetime
-from .. import logger, ch
 from ... import logFmt
-from ...definitions import RESULT_DIR
 from ...functions import runOnelab, calcIronLoss, importResults
 from ...script.geometry.machineAllType import MachineAllType
 from ...script.geometry.rotor import Rotor
 from ...script.geometry.stator import Stator
 from ...script.script import Script
 from ...script.material import ElectricalSteel
+from .. import logger
 from . import boundaryJSON, importJSON, modelJSON, apiNameDict
 from .SurfaceJSON import SurfaceAPI
 
@@ -430,7 +429,7 @@ def main(
 
     # generate the machine geometry
     machine, machineSurfDict = createMachine(segmentSurfDict, extendedInfo)
-    
+
     # set function mesh
     if "useFunctionMesh" in extendedInfo.keys():
         if extendedInfo["useFunctionMesh"]:
@@ -600,76 +599,3 @@ def main(
                         showfig=False,
                         savePath=None,
                     )
-
-
-if __name__ == "__main__":
-    # 1. Check that all argvs are valid!
-    parser = argparse.ArgumentParser(
-        description="Process Motor-JSON files to generate a Onelab Simulation."
-    )
-    parser.add_argument(
-        "geo",
-        help="path to the JSON geometry file ('geometry.json')",
-        type=str,
-    )
-    parser.add_argument(
-        "extInfo",
-        help="path to the extended info JSON file ('extendedInfo.json')",
-        type=str,
-    )
-    parser.add_argument(
-        "--gmsh",
-        help="path to the Gmsh executable",
-        type=str,
-        default="",
-    )
-    parser.add_argument(
-        "--getdp", help="path to the GetDP executable", type=str, default=""
-    )
-    parser.add_argument(
-        "--mod",
-        help="path where the model files should be stored",
-        type=str,
-        default=RESULT_DIR,
-    )
-    parser.add_argument(
-        "--res",
-        help="path where the simulation results should be stored",
-        type=str,
-        default="",
-    )
-
-    parser.add_argument(
-        "--log",
-        help="logging level for execution. Options are: error, warning, info, debug. default is warning",
-        type=str,
-        default="WARNING",
-    )
-
-    parser.add_argument(
-        "-v",
-        help="set execution to verbose. Verbosity level equals logging level.",
-        action=argparse.BooleanOptionalAction,
-    )
-
-    args = parser.parse_args()
-
-    # remove commandline handler if verbose
-    if args.v:
-        logger.removeHandler(ch)
-
-    # set log level
-    loglevel = args.log
-    logLevelNum = getattr(logging, loglevel.upper(), None)
-    if not isinstance(logLevelNum, int):
-        raise ValueError(f"Invalid log level: {loglevel}")
-    logger.setLevel(logLevelNum)
-
-    main(
-        geo=args.geo,
-        extInfo=args.extInfo,
-        gmsh=args.gmsh,
-        getdp=args.getdp,
-        model=args.mod,
-        results=args.res,
-    )
