aa79378e11a4e390dc20d36faa6a3d0788072083
Max Schuler <max.schuler@thws.de>
2024-04-30 16:42:02 +0200

    [WP] simple gmsh test working, but gmsh exe is set wrong

--- tests/script/test_gmsh.py
+++ tests/script/test_gmsh.py
@@ -1,14 +1,9 @@
-import pytest
 import os
 import subprocess
 from pyemmo.functions.runOnelab import createGMSHCommand
 from tests import GMSH_EXE, TEST_DATA_DIR
 
 
-# @pytest.mark.parametrize("test_name,corrected_name",[
-#     ('test#name','test_name'),
-#     ('0testname','_0testname'),
-# ])
 def test_run_gmsh():
     """Simple test to check if gmsh executable is set correctly by automatic
     installation and mesh command is correct and runs."""
@@ -18,9 +13,8 @@ def test_run_gmsh():
         gmshFile=geo_file, useGUI=False, gmshPath=GMSH_EXE, logFileName=""
     )
     out = subprocess.run(
-        gmsh_command, check=False, capture_output=True, text=True
+        gmsh_command, check=True, capture_output=True, text=True
     )
-    print(out)
 
 
 if __name__ == "__main__":
