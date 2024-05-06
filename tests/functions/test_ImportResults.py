db1d33ec268f27a90f56261138747711abcc7d09
Vaibhav Patil <vaibhav.patil@study.thws.de>
2024-05-06 15:00:28 +0200

    [NF] : New tests have been added.

--- tests/functions/test_ImportResults.py
+++ tests/functions/test_ImportResults.py
@@ -11,7 +11,10 @@ import gmsh
 
 from pyemmo.functions.import_results import read_timetable_dat
 from pyemmo.functions.import_results import read_RegionValue_dat
+from pyemmo.functions.import_results import split_data
+from pyemmo.functions.import_results import plot_timetable_dat
 import pytest
+#from .. import TEST_DATA_DIR
 
 
 @pytest.mark.parametrize(
@@ -23,7 +26,7 @@ import pytest
     ],
 )
 def test_file_exists(file_path):
-    """Tests if a File not Found Error is raised"""
+    """Tests if a File not Found Error is raised."""
     try:
         read_timetable_dat(file_path)
         os.path.isfile(
@@ -44,7 +47,7 @@ def test_file_exists(file_path):
     ],
 )
 def test_datasize_values(file_path):
-    """Tests of the size and values of the data array are correct"""
+    """Tests if the size and values of the data array are correct."""
     read_timetable_dat(file_path)
     test_data_array = np.loadtxt(file_path, dtype=float, comments="#")
 
@@ -59,17 +62,64 @@ def test_datasize_values(file_path):
         test_data_array, assert_data_array
     ), " data values are incorrect!"
 
-
 @pytest.mark.parametrize(
     "file_path",
     [
         (
+            #os.path.join(TEST_DATA_DIR, "functions","import_results","data_file.dat"),
             "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Surf_StLu2.dat"
         )
     ],
 )
 def test_checkImportedValues(file_path):
+    """Tests if the Time and Data Values are imported correctly from dat-file in Region Value format."""
+    data_array = read_RegionValue_dat(file_path)
+    time_array=np.ndarray((1,), buffer=np.array([0.001666666666666667]), dtype=float)
+    value_array=np.ndarray((1,), buffer=np.array([7.609826513417671e-05]), dtype=float)
+    
+    test_tuple =(time_array,value_array)
 
-    test_array = read_RegionValue_dat(file_path)
-    assert_arrray = np.array((0.001666666666666667, 7.609826513417671e-05))
-    assert np.array_equal(test_array, assert_arrray)
+    
+    assert np.array_equal(data_array,test_tuple)
+
+
+
+def test_split_data():
+    
+    time = np.ndarray((5,), buffer=np.array([0.,1.,2.,0.,1.]), dtype=float)
+    values = np.ndarray((5,), buffer=np.array([0.1,1.1,2.1,0.2,1.2]), dtype=float)
+    
+
+    
+    test_tuple=split_data(time,values)
+    
+    for x in range(np.size(time)):
+          
+        if(time[x]==0):
+            assert time[x+1] != 0,"There is a same time step, twice in a row!"
+            
+          
+
+
+    nbr_sims = int 
+    nbr_sims = 2
+
+    assert_time1 = [(np.ndarray((3,), buffer=np.array([0.,1.,2.]), dtype=float)),(np.ndarray((2,), buffer=np.array([0.,1.]), dtype=float))]
+    assert_values1 = [(np.ndarray((3,), buffer=np.array([0.1,1.1,2.1]), dtype=float)),(np.ndarray((2,), buffer=np.array([0.2,1.2]), dtype=float))]
+    
+    asserttuple=(nbr_sims,assert_time1,assert_values1)
+    assert np.array_equal(asserttuple,test_tuple)
+
+def test_plot_timetable_dat():
+    """Tests that the number offigures created is equal to the number of Simulations"""
+    file_path = "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Surf_StLu2.dat"
+    Plot_list = plot_timetable_dat( file_path, "DataLabel","Graph",False,True,None )
+    number_of_figures =1
+    assert len(Plot_list) == number_of_figures, " The number of figures created is not equal to the number of Simulations!"
+
+
+
+if __name__=="__main__":
+    test_split_data()
+
+ 
