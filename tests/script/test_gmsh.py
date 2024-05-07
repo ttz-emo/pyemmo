fbae7924f799fc03e60123cb7a9b37775bcf199a
Max Schuler <max.schuler@thws.de>
2024-05-07 13:25:25 +0200

    [CO] added removal of msh and db file created by gmsh test

--- tests/script/test_gmsh.py
+++ tests/script/test_gmsh.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join
 import subprocess
 from pyemmo.functions.runOnelab import createGMSHCommand
 from tests import GMSH_EXE, TEST_DATA_DIR
@@ -7,7 +8,8 @@ from tests import GMSH_EXE, TEST_DATA_DIR
 def test_run_gmsh():
     """Simple test to check if gmsh executable is set correctly by automatic
     installation and mesh command is correct and runs."""
-    geo_file = os.path.join(TEST_DATA_DIR, "MACHINE_TEST.geo")
+    model_name = "MACHINE_TEST"
+    geo_file = join(TEST_DATA_DIR, model_name + ".geo")
     assert os.path.isfile(geo_file)
     gmsh_command = createGMSHCommand(
         gmshFile=geo_file, useGUI=False, gmshPath=GMSH_EXE, logFileName=""
@@ -15,8 +17,14 @@ def test_run_gmsh():
     out = subprocess.run(
         gmsh_command, check=True, capture_output=True, text=True
     )
-    # TODO: Check that .db and .msh file are created and clean them up after
-    # test run
+    for res_ext in (".db", ".msh"):
+        res_file = join(TEST_DATA_DIR, model_name + res_ext)
+        if os.path.isfile(res_file):
+            os.remove(res_file)
+        else:
+            raise FileNotFoundError(
+                f"Coult not find gmsh result file: {res_file}"
+            )
 
 
 if __name__ == "__main__":
