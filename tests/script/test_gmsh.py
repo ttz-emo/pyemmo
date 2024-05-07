8508957bb412530262142cd9fc1512f32083312c
Max Schuler <max.schuler@thws.de>
2024-05-07 13:35:55 +0200

    [CC] added copyright and module docstring for gmsh exe test

--- tests/script/test_gmsh.py
+++ tests/script/test_gmsh.py
@@ -1,6 +1,42 @@
+#
+# Copyright (c) 2018-2024 M. Schuler, TTZ-EMO, Technical University of Applied Sciences Wuerzburg-Schweinfurt.
+#
+# This file is part of PyEMMO
+# (see https://gitlab.ttz-emo.thws.de/ag-em/pyemmo).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Test module for automatic onelab installation by install_onelab.ps1 script.
+
+The PowerShell script downloads the latest onelab version from the onelab
+website (https://onelab.info/files) and installs it in
+"tests/data/YYMMDD_onelab".
+This script is called by tests/__init__.py and sets the gmsh and getdp
+executable paths through the files "GMSH_TEST_PATH" and "GETDP_TEST_PATH" in
+the tests directory.
+
+This module uses the new gmsh installation for the tests and runs it on a
+sample machine script ("tests/data/MACHINE_TEST.geo") to make sure the actual
+gmsh version does work with the .geo files created by PyEMMO.
+TODO: Create the test.geo machine script automatically to make sure that really
+the latest PyEMMO version is tested.
+"""
 import os
 from os.path import join
 import subprocess
+import logging
 from pyemmo.functions.runOnelab import createGMSHCommand
 from tests import GMSH_EXE, TEST_DATA_DIR
 
@@ -8,6 +44,11 @@ from tests import GMSH_EXE, TEST_DATA_DIR
 def test_run_gmsh():
     """Simple test to check if gmsh executable is set correctly by automatic
     installation and mesh command is correct and runs."""
+    if not GMSH_EXE:
+        logging.warning(
+            "Skipping gmsh exe tests because GMSH_EXE was not set for tests"
+        )
+        return None
     model_name = "MACHINE_TEST"
     geo_file = join(TEST_DATA_DIR, model_name + ".geo")
     assert os.path.isfile(geo_file)
