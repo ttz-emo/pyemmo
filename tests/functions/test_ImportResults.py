12cd54a5b58bbd88a3ad0ec78e515d1c4b759c7c
Vaibhav Patil <vaibhav.patil@study.thws.de>
2024-05-08 14:48:25 +0200

    [NF]:New tests were added for import_results

--- tests/functions/test_ImportResults.py
+++ tests/functions/test_ImportResults.py
@@ -13,15 +13,22 @@ from pyemmo.functions.import_results import read_timetable_dat
 from pyemmo.functions.import_results import read_RegionValue_dat
 from pyemmo.functions.import_results import split_data
 from pyemmo.functions.import_results import plot_timetable_dat
+from pyemmo.functions.import_results import importSP
 import pytest
-#from .. import TEST_DATA_DIR
 
+#from .. import TEST_DATA_DIR
+TEST_DATA_DIR = r"C:\Users\k54149\Documents\pyemmo"
 
 @pytest.mark.parametrize(
     "file_path",
     [
-        (
-            "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Ib.dat"
+        os.path.join(
+            TEST_DATA_DIR,
+            "Results",
+            "Baukasten",
+            "Test_SPMSM",
+            "res_Test_SPMSM_Baukasten",
+            "Ib.dat",
         )
     ],
 )
@@ -41,8 +48,13 @@ def test_file_exists(file_path):
 @pytest.mark.parametrize(
     "file_path",
     [
-        (
-            "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Ib.dat"
+        os.path.join(
+            TEST_DATA_DIR,
+            "Results",
+            "Baukasten",
+            "Test_SPMSM",
+            "res_Test_SPMSM_Baukasten",
+            "Ib.dat",
         )
     ],
 )
@@ -62,64 +74,124 @@ def test_datasize_values(file_path):
         test_data_array, assert_data_array
     ), " data values are incorrect!"
 
+
 @pytest.mark.parametrize(
     "file_path",
     [
-        (
-            #os.path.join(TEST_DATA_DIR, "functions","import_results","data_file.dat"),
-            "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Surf_StLu2.dat"
-        )
+        os.path.join(
+            TEST_DATA_DIR,
+            "Results",
+            "Baukasten",
+            "Test_SPMSM",
+            "res_Test_SPMSM_Baukasten",
+            "Surf_StLu2.dat",
+        ),
+        # "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Surf_StLu2.dat"
     ],
 )
-def test_checkImportedValues(file_path):
+def test_Region_value(file_path):
     """Tests if the Time and Data Values are imported correctly from dat-file in Region Value format."""
     data_array = read_RegionValue_dat(file_path)
-    time_array=np.ndarray((1,), buffer=np.array([0.001666666666666667]), dtype=float)
-    value_array=np.ndarray((1,), buffer=np.array([7.609826513417671e-05]), dtype=float)
-    
-    test_tuple =(time_array,value_array)
+    time_array = np.ndarray(
+        (1,), buffer=np.array([0.001666666666666667]), dtype=float
+    )
+    value_array = np.ndarray(
+        (1,), buffer=np.array([7.609826513417671e-05]), dtype=float
+    )
 
-    
-    assert np.array_equal(data_array,test_tuple)
+    test_tuple = (time_array, value_array)
 
+    assert np.array_equal(data_array, test_tuple),"Time and Data Values were imported incorrectly!"
 
 
 def test_split_data():
-    
-    time = np.ndarray((5,), buffer=np.array([0.,1.,2.,0.,1.]), dtype=float)
-    values = np.ndarray((5,), buffer=np.array([0.1,1.1,2.1,0.2,1.2]), dtype=float)
-    
 
-    
-    test_tuple=split_data(time,values)
-    
+    time = np.ndarray(
+        (5,), buffer=np.array([0.0, 1.0, 2.0, 0.0, 1.0]), dtype=float
+    )
+    values = np.ndarray(
+        (5,), buffer=np.array([0.1, 1.1, 2.1, 0.2, 1.2]), dtype=float
+    )
+
+    test_tuple = split_data(time, values)
+
     for x in range(np.size(time)):
-          
-        if(time[x]==0):
-            assert time[x+1] != 0,"There is a same time step, twice in a row!"
-            
-          
 
+        if time[x] == 0:
+            assert (
+                time[x + 1] != 0
+            ), "There is a same time step, twice in a row!"
 
-    nbr_sims = int 
+    nbr_sims = int
     nbr_sims = 2
 
-    assert_time1 = [(np.ndarray((3,), buffer=np.array([0.,1.,2.]), dtype=float)),(np.ndarray((2,), buffer=np.array([0.,1.]), dtype=float))]
-    assert_values1 = [(np.ndarray((3,), buffer=np.array([0.1,1.1,2.1]), dtype=float)),(np.ndarray((2,), buffer=np.array([0.2,1.2]), dtype=float))]
-    
-    asserttuple=(nbr_sims,assert_time1,assert_values1)
-    assert np.array_equal(asserttuple,test_tuple)
+    assert_time1 = [
+        (np.ndarray((3,), buffer=np.array([0.0, 1.0, 2.0]), dtype=float)),
+        (np.ndarray((2,), buffer=np.array([0.0, 1.0]), dtype=float)),
+    ]
+    assert_values1 = [
+        (np.ndarray((3,), buffer=np.array([0.1, 1.1, 2.1]), dtype=float)),
+        (np.ndarray((2,), buffer=np.array([0.2, 1.2]), dtype=float)),
+    ]
+
+    asserttuple = (nbr_sims, assert_time1, assert_values1)
+    assert (
+        nbr_sims == test_tuple[0]
+    ), " Incorrect number of  Simulations imported! "
+
+    assert np.array_equal(
+        assert_time1[0], test_tuple[1][0]
+    ), " Incorrect timesteps imported!"
+    assert np.array_equal(
+        assert_time1[1], test_tuple[1][1]
+    ), "Incorrect timesteps imported!"
+
+    assert np.array_equal(
+        assert_values1[0], test_tuple[2][0]
+    ), "Incorrect Values imported!"
+    assert np.array_equal(
+        assert_values1[1], test_tuple[2][1]
+    ), "Incorrect Values imported!"
+
 
 def test_plot_timetable_dat():
     """Tests that the number offigures created is equal to the number of Simulations"""
-    file_path = "C:/Users/k54149/Documents/pyemmo/Results/Baukasten/Test_SPMSM/res_Test_SPMSM_Baukasten/Surf_StLu2.dat"
-    Plot_list = plot_timetable_dat( file_path, "DataLabel","Graph",False,True,None )
-    number_of_figures =1
-    assert len(Plot_list) == number_of_figures, " The number of figures created is not equal to the number of Simulations!"
+    file_path = os.path.join(TEST_DATA_DIR,"Results","Baukasten","Test_SPMSM","res_Test_SPMSM_Baukasten","Surf_StLu2.dat")
+    Plot_list = plot_timetable_dat(
+        file_path, "DataLabel", "Graph", False, True, None
+    )
+    number_of_figures = 1
+    assert (
+        len(Plot_list) == number_of_figures
+    ), " The number of figures created is not equal to the number of Simulations!"
 
 
+def test_import_SP():
+    """Tests the Data-name,Time vector,Postion array and Value array of the imported .pos file"""
+    file_path = os.path.join(TEST_DATA_DIR,"Results","Baukasten","Test_SPMSM","res_Test_SPMSM_Baukasten","btan.pos")
+    test_tuple = importSP(file_path)
+    assert_tuple = (
+        "b_tangent",
+        [],
+        [
+            (0.06349364999999998, 0.0, 0.0),
+            (0.0634912323563634, 0.0005540795906463301, 0.0),
+            (0.06348397960956659, 0.001108116985986626, 0.0),
+        ],
+        [
+            [-0.05582340238180805],
+            [-0.05609076949641559],
+            [-0.05854188999837832],
+        ],
+    )
 
-if __name__=="__main__":
-    test_split_data()
+    assert np.array_equal(test_tuple[0], assert_tuple[0])," Incorrect Time-vector Imported!"
+    for x in range(len(assert_tuple[2]) - 1):
+        assert np.array_equal(test_tuple[2][x], assert_tuple[2][x]),"Incorrect Postion-Array Imported!"
 
- 
+    for x in range(len(assert_tuple[3]) - 1):
+        assert np.array_equal(test_tuple[3][x], assert_tuple[3][x]);"Incorrect Value-Array Imported! "
+
+
+# if __name__ == "__main__":
+#     test_checkImportedValues(TEST_DATA_DIR)
