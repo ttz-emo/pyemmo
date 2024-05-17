967dec46041520248c229fbd50c25e14fee7dcff
Max Schuler <max.schuler@thws.de>
2024-05-17 13:36:57 +0200

    [WP] added geo and pro files as asm dev to tracking

--- workingDirectory/test_asm/definitions/__init__.py
+++ workingDirectory/test_asm/definitions/__init__.py
@@ -2,7 +2,7 @@ import os
 from pyemmo.definitions import ROOT_DIR
 
 # Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz
-MODEL_NAME = "Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz"
+MODEL_NAME = "1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz"
 MODEL_DIR = os.path.abspath(
     os.path.join(
         ROOT_DIR,
