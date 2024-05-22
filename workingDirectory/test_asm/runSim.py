ad38ddc9c40ac1046590c8fba5903652330a15a5
Max Schuler <max.schuler@thws.de>
2024-05-22 16:21:31 +0200

    [CC] adapted runSim script to blocked rotor updates

--- workingDirectory/test_asm/runSim.py
+++ workingDirectory/test_asm/runSim.py
@@ -8,7 +8,12 @@ from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 import numpy as np
 from pyemmo.functions.runOnelab import runCalcforCurrent
-from pyemmo.functions.import_results import read_timetable_dat, plot_all_dat
+from pyemmo.functions.import_results import (
+    read_timetable_dat,
+    plot_all_dat,
+    plot_timetable_dat,
+    read_RegionValue_dat,
+)
 from definitions import MODEL_NAME, MODEL_DIR
 
 class NumpyEncoder(json.JSONEncoder):
@@ -55,24 +60,33 @@ logging.debug("Timestep %e s.", timestep)
 logging.debug("One time step equals %f° mechanical degrees.", winkelschritt)
 logging.debug("Stop time of simulation: %.7e s", int(nbrSteps) * timestep)
 # %%
-resId = f"test + {time.ctime()}"
+resId = f"blockedRotor_{n}rpm_{f_r}Hz_{nbr_stator_periods}Periods"
 paramDict = {
     "getdp": {
         "freq_rotor": f_r,
         "I_eff": I_eff,
         # "IQ_RMS": float(idq[1]),
         "RPM": float(n),
-        "nbStatorPeriods": nbr_stator_periods,
+        "initrotor_pos": 0.0,
+        "nbrStatorPeriods": nbr_stator_periods,
         "d_theta": winkelschritt,
         "ResId": resId,
         "Flag_AnalysisType": 1,
         "Flag_PrintFields": 0,
-        "Flag_Debug": 0,
-        "Flag_ClearResults": 1,
-        "verbosity level": 5,
-        "L_endring_segment": 2e-9,
-        "R_endring_segment": 1e-16,
-        "msh": os.path.join(MODEL_DIR, "coarseMesh.msh"),  # fineMesh.msh
+        "Flag_Debug": 1,
+        "Flag_ClearResults": 0,
+        "verbosity level": 3,
+        # "AxialLength_R": 1,
+        # "AxialLength_S": 1,
+        "NbrParallelPaths": 1,
+        "R_endring_segment": 16e-7 / 2,  # Initial value: 16e-7,
+        "L_endring_segment": 2e-9 / 2,
+        "Flag_Cir_RotorCage": 1,
+        "Flag_Dynamic_RotorBarResistance": 0,
+        # "Flag_Calculate_VW": 1,
+        #                          fineMesh or coarseMesh
+        "msh": os.path.join(MODEL_DIR, "fineMesh.msh"),
+        # "Flag_SecondOrder": 0,
     },
     "ResId": resId,
     "pro": os.path.join(MODEL_DIR, MODEL_NAME + ".pro"),
@@ -87,9 +101,9 @@ paramDict = {
     # "exc": 0,
     # "axLen": 0.2,
     # "sym": 4,
-    "info": "Reset speed to 1000 rpm.",
-    "PostOp": ["test", "GetBOnRadius"],
+    "info": "Neue Simulation bei 1000 rpm, 4Hz mit statischem Rotorstab Widerstand.",
     "datetime": time.ctime(),
+    "PostOp": [],
 }
 
 results = runCalcforCurrent(paramDict)
@@ -200,6 +214,84 @@ ax.set_xlim(14.5, 15)
 ax.legend()
 # ax.set_ylim(-1e-5,1e-5)
 ax.grid(True)
+# %%
+# Plot Drehmoment
+fig, ax = plt.subplots()
+ax: Axes = ax
+if os.path.isfile(os.path.join(respath, "Ts_vw.dat")):
+    t_t, ts_vw = read_RegionValue_dat(os.path.join(respath, "Ts_vw.dat"))
+    _, tr_vw = read_RegionValue_dat(os.path.join(respath, "Tr_vw.dat"))
+
+    # all Torques
+    # line_ts = ax.plot(results["time"], results["torque"]["stator"], label=f"Ts (MST)")
+    # line_tr = ax.plot(results["time"], results["torque"]["rotor"], label=f"Tr (MST)")
+    # ax.plot(t_t, ts_vw, label=f"Ts (VW)")
+    # ax.plot(t_t, tr_vw, label=f"Tr (VW)")
+    ax.plot(t_t, np.mean([ts_vw, tr_vw], axis=0), label="VW")
+
+# mean torque
+results["torque"]["mean"] = np.mean(
+    [results["torque"]["stator"], results["torque"]["rotor"]], axis=0
+)
+ax.plot(
+    results["time"],
+    results["torque"]["mean"],
+    ".-",
+    label="MST",
+)
+# ax.set_ylabel("Spannung in V")
+ax.legend(loc=1)
+ax.set_xlim(
+    t[int(t.size * (nbr_stator_periods - 1) / nbr_stator_periods)], t[-1]
+)
+ax.set_ylim([-25,60]) # medium zoom
+# ax.set_ylim([210, 235])  # high zoom
+ax.set_title("Drehmoment")
+ax.grid(True)
+# %%
+# PLOT RESISTANCES
+# %matplotlib widget
+R_bar_dc = 5.19932563e-05
+fig, ax = plt.subplots()
+ax: Axes = ax
+# for nBar in range(1,nbr_bars+1):
+for nBar in (1, 5, 9):
+    resfile = os.path.join(respath, f"R_bar_{nBar}.dat")
+    if os.path.isfile(resfile):
+        t, R_bar = read_timetable_dat(resfile)
+        ax.plot(t, R_bar, label=f"R_bar_{nBar}", marker=".")
+ax.set_ylim(bottom=0, top=3 * R_bar_dc)
+ax.legend()
+# plt.close()
+# plot_timetable_dat(resfile,dataLabel=f"R_{{bar,{nBar}}}")
+# %%
+# Export Data for Maxwell
+from pyemmo.functions.exportMaxwell import exportTabMaxwell
+
+# Export Bar Current
+f_name_mxwl_export = os.path.join(respath, "I_Bar_1.tab")
+if not os.path.isfile(f_name_mxwl_export):
+    exportTabMaxwell(
+        [out_dict["time"], I_bars[:, 1]],
+        identifier=["Time (s)", "I_Bar_Onelab (A)"],
+        filepath=f_name_mxwl_export,
+    )
+# Export Bar Voltage
+f_name_mxwl_export = os.path.join(respath, "U_Bar_1.tab")
+if not os.path.isfile(f_name_mxwl_export):
+    exportTabMaxwell(
+        [out_dict["time"], U_bars[:, 1]],
+        identifier=["Time (s)", "V_Bar_Onelab (V)"],
+        filepath=f_name_mxwl_export,
+    )
+# Export Torque
+f_name_mxwl_export = os.path.join(respath, "torque.tab")
+if not os.path.isfile(f_name_mxwl_export):
+    exportTabMaxwell(
+        [out_dict["time"], results["torque"]["mean"]],
+        identifier=["Time (s)", "Torque_Onelab (Nm)"],
+        filepath=f_name_mxwl_export,
+    )
 # %%
 logging.shutdown()
 # %% FINAL
