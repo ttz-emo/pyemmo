95fd28f7cce43f188afecb11f1890f97e15d915e
Max Schuler <max.schuler@thws.de>
2024-05-24 15:09:40 +0200

    [CO] added numpy min max function instead of python internal because it gives single value if all values are 0.0

--- pyemmo/functions/import_results.py
+++ pyemmo/functions/import_results.py
@@ -189,17 +189,18 @@ def plot_timetable_dat(
         sim_data = dataArray[sim]
         fig, axes = plt.subplots()
         fig.set_dpi(200)
-        axes.plot(timeArray[sim], sim_data)#, marker=".")
+        axes.plot(timeArray[sim], sim_data)  # , marker=".")
         # show()
         # ax.set_aspect("equal", adjustable="box")
         # check that max or min is not too close to zero to apply the y_lim
         if 1 in sim_data.shape:
-            maxVal = max(sim_data)
-            minVal = min(sim_data)
+            maxVal = np.max(sim_data)
+            minVal = np.min(sim_data)
         else:
             # multi data array
-            maxVal = max(sim_data[:,0]) # FIXME: Make sure second axis is really data axis
-            minVal = min(sim_data[:,0])
+            # FIXME: Make sure second axis is really data axis
+            maxVal = np.max(sim_data[:, 0])
+            minVal = np.min(sim_data[:, 0])
         if not (
             isclose(maxVal, 0, abs_tol=0.1) or isclose(minVal, 0, abs_tol=0.1)
         ):
@@ -207,7 +208,7 @@ def plot_timetable_dat(
                 bottom=minVal * (1.1 if minVal < 0 else 0.9),
                 top=maxVal * (1.1 if maxVal > 0 else 0.9),
             )
-        
+
         axes.set(ylabel=dataLabel, xlabel="time in s", title=title + f"_{sim}")
         if savefig:
             if not savePath:
