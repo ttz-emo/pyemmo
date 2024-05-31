0f4719b8c9281403da7dbbdcc568684f7e88f290
Vu Nguyen <vu.nguyen@student.fhws.de>
2024-05-31 12:53:26 +0200

    1st commit of Vu's WIP test files

---
+++ workingDirectory/Vu/pyleecan_test_base.py
@@ -0,0 +1,74 @@
+import os
+import logging
+
+from pyleecan.Functions import load
+from pyleecan.Classes.Machine import Machine
+
+from pyemmo.definitions import ROOT_DIR
+from pyemmo.api.pyleecan import main as pyleecanAPI
+
+from pyemmo.functions.runOnelab import runCalcforCurrent, findGmsh, findGetDP
+
+test_cases = {
+    1: "IPMSM_B",
+}
+
+test_params = {
+    1: {
+        "id": -10,
+        "iq": 50,
+        "rpm": 1000,
+        "d_theta": 0.25,
+    }
+}
+
+
+def pyleecan_test_base(test_cases: dict, test_params: list, print_flag: bool = 0, debug_flag: bool = 0) :
+
+    # disable messages of matplotlib
+    logging.getLogger("matplotlib.font_manager").setLevel(logging.ERROR)
+    logging.getLogger().setLevel(logging.INFO)
+
+    for k, v in test_cases.items():
+        machine_file_path = os.path.join(
+            ROOT_DIR, f"tests\data\\api\pyleecan\{v}.json"
+        )
+        assert os.path.isfile(machine_file_path)  # make sure file exists
+
+        pyleecan_machine: Machine = load.load(machine_file_path)
+        resFolder = os.path.join(ROOT_DIR, f"Results\pyleecanAPI\\test_{k}\\{pyleecan_machine.name}")
+        if not os.path.isdir(resFolder):
+            os.makedirs(resFolder)
+        pyleecanAPI.main(pyleecan_machine, model_dir = resFolder)
+        geo_file = os.path.join(resFolder, pyleecan_machine.name + ".geo")
+        pro_file = os.path.join(resFolder, pyleecan_machine.name + ".pro")
+
+        sim_res_dir = os.path.join(resFolder, f"res_{pyleecan_machine.name}")
+        if not os.path.isdir(sim_res_dir):
+            os.makedirs(sim_res_dir)
+    
+        id = test_params["id"]
+        iq = test_params["iq"]
+        n = test_params["rpm"]
+        resid = f"id_{id}_iq_{iq}_n_{n}rpm"
+        param_dict = {
+            "getdp": {
+                "ID_RMS": id,
+                "IQ_RMS": iq,
+                "RPM": n,
+                "d_theta": test_params["d_theta"],
+                "ResId": resid,
+                "Flag_PrintFields": print_flag,
+                "Flag_Debug": debug_flag,
+            },
+            "ResId": resid,
+            "pro": pro_file,
+            "res": sim_res_dir,
+            # "exe": findGetDP(),
+            "exe": r'H:\onelab-Windows64\getdp.exe',
+            "gmsh": findGmsh(),
+        }
+        # runCalcforCurrent(param_dict)
+
+if __name__ == "__main__":
+    pyleecan_test_base(test_cases, test_params)
---
+++ workingDirectory/Vu/test_pytest.py
@@ -0,0 +1,52 @@
+from pyleecan_test_base import pyleecan_test_base
+import os
+import glob
+from collections import defaultdict
+from pyemmo.definitions import ROOT_DIR
+
+import configparser
+from datetime import datetime
+
+# Overwrite log_file filename in pytest.ini with datetime
+def update_py_test_ini():
+    curr_datetime = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+
+    test_config = configparser.ConfigParser()
+    test_config.read('pytest.ini')
+    # Update the string at log_file to with latest datetime
+    test_config.set('pytest', 'log_file', 'logs/pytest-{}-logs.log'.format(curr_datetime))
+
+    with open('pytest.ini', 'w') as configfile:
+        test_config.write(configfile)
+
+
+class TestCases:
+    test_cases = defaultdict(dict)
+
+    test_params = {
+        'api\pyleecan': {
+            "id": -10,
+            "iq": 50,
+            "rpm": 1000,
+            "d_theta": 0.25,
+        }
+    }
+    def test_pyleecan(self):
+        test_type = "api\pyleecan"
+        self.make_test_cases(test_type)
+        pyleecan_test_base(self.test_cases[test_type], self.test_params[test_type])
+
+        for k, v in self.test_cases[test_type].items():
+            assert len(glob.glob(os.path.join(ROOT_DIR, f"Results\pyleecanAPI\\test_{k}\\{v}\\*.geo"))) == 2
+
+    def test_dummy(self):
+        assert 1 + 1 == 3 
+
+    def make_test_cases(self, test_type: str) -> dict:
+        test_files = glob.glob(os.path.join(ROOT_DIR, r"tests\data", test_type, "*.json"))
+        test_names = list(map(lambda x: x.split(".")[0], map(lambda x: x.split("\\")[-1], test_files)))
+        self.test_cases[test_type] = {}
+        for id, name in enumerate(test_names):
+             self.test_cases[test_type][id] = name
+        
+        return self.test_cases
