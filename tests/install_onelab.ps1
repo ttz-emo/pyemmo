8a258a08ab57898592b4e4cba77b630cfe7f0669
Max Schuler <max.schuler@thws.de>
2024-06-06 12:59:45 +0200

    [WP] only setting environ variables when onelab not installed since we are using the file import anyway

--- tests/install_onelab.ps1
+++ tests/install_onelab.ps1
@@ -44,25 +44,25 @@ if (Test-Path $store_path) {
 }
 else {
     New-Item -Path $store_path -ItemType Directory
-}
 
-$zip_filepath = $store_path + "\onelab.zip"
-Write-Output("Zip file path is: $zip_filepath")
-if (Test-Path $zip_filepath) {
-    Write-Output("onelab.zip has allready been downloaded.")
-}
-else {
+    $zip_filepath = $store_path + "\onelab.zip"
+    Write-Output("Zip file path is: $zip_filepath")
+    if (Test-Path $zip_filepath) {
+        Write-Output("onelab.zip has allready been downloaded.")
+    }
+    else {
     (New-Object System.Net.WebClient).DownloadFile('https://onelab.info/files/onelab-Windows64.zip', $zip_filepath)
+    }
+    $onelab_path = Join-Path -Path $store_path  -ChildPath "onelab-Windows64"
+    if (Test-Path $onelab_path) {
+        Write-Output "onelab.zip allready expanded."
+    }
+    else {
+        Expand-Archive ($zip_filepath) -DestinationPath $store_path
+        Remove-Item  $zip_filepath
+    }
+    # $env:GMSH_TEST_PATH = "$store_path\onelab-Windows64\gmsh.exe"
+    [System.Environment]::SetEnvironmentVariable('GMSH_TEST_PATH', "$store_path\onelab-Windows64\gmsh.exe", 'User')
+    $env:GETDP_TEST_PATH = "$store_path\onelab-Windows64\getdp.exe"
 }
-$onelab_path = Join-Path -Path $store_path  -ChildPath "onelab-Windows64"
-if (Test-Path $onelab_path){
-    Write-Output "onelab.zip allready expanded."
-}
-else{
-    Expand-Archive ($zip_filepath) -DestinationPath $store_path
-    Remove-Item  $zip_filepath
-}
-# $env:GMSH_TEST_PATH = "$store_path\onelab-Windows64\gmsh.exe"
-[System.Environment]::SetEnvironmentVariable('GMSH_TEST_PATH', "$store_path\onelab-Windows64\gmsh.exe", 'User')
-$env:GETDP_TEST_PATH = "$store_path\onelab-Windows64\getdp.exe"
 Set-Location ..
