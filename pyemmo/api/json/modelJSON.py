08ae1b55d9012e2136c8db1505736eec648b03af
Max Schuler <max.schuler@thws.de>
2024-06-13 16:47:19 +0200

    [CO] fixed nbrSlots in coil span calculation

--- pyemmo/api/json/modelJSON.py
+++ pyemmo/api/json/modelJSON.py
@@ -814,7 +814,7 @@ def createWinding(extendedInfo: dict) -> datamodel:
     swatemWinding.set_phases(
         S=windLayout,
         turns=(importJSON.getNbrOfTurns(extendedInfo)),
-        w=get_min_coilspan(windLayout),
+        w=get_min_coilspan(windLayout, nbrSlots),
     )
     swatemWinding.analyse_wdg()  # analyse winding to make sure its valid and all parameters are set
     # make sure that number of parallel paths does not exceed max. possible paths of winding:
