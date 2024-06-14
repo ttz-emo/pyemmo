07d094a3c375229add837cdf30e5fe7e5a8df168
Max Schuler <max.schuler@thws.de>
2024-06-14 14:16:54 +0200

    [BF] fixed config file path + black [CC] pyupgrade hook fixed open file with -r

--- workingDirectory/Vu/integrationTest/testUtils.py
+++ workingDirectory/Vu/integrationTest/testUtils.py
@@ -3,44 +3,52 @@ import os
 import glob
 from collections import defaultdict
 from datetime import datetime
-
+from pyemmo.definitions import ROOT_DIR
 import re
 
 
-
 def updateConfig(test_type: str, test_id: int, test_case: str):
-    
-    '''
+    """
     Function to update config file to generate proper test file name
-    '''
+    """
 
     curr_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
     parser = ConfigParser()
-    config_path = os.path.join(os.path.abspath('.'), "pytest.ini")
+    config_path = os.path.join(
+        ROOT_DIR, "workingDirectory", "Vu", "pytest.ini"
+    )
     parser.read(config_path)
-    dest_name = f"logs/{test_type}/test_{test_id}_{test_case}/{curr_datetime}.log"
-    parser.set('pytest', 'log_file', dest_name)
+    dest_name = (
+        f"logs/{test_type}/test_{test_id}_{test_case}/{curr_datetime}.log"
+    )
+    parser.set("pytest", "log_file", dest_name)
 
-    with open(config_path, 'w+') as config_file:
+    with open(config_path, "w+") as config_file:
         parser.write(config_file)
     return curr_datetime, dest_name
 
+
 import os
+
 
 def messagePrinter(msg: str = "Assert OK"):
     print(msg)
     return True
 
+
 def count_files(folder_path: str) -> dict:
-    '''
+    """
     Function to count files of each type in a path.
     Args: folder_path: path to count in. Needs to be abs path to the dest folder
-    Output: n-items Dict 
-        - 1st entry being folder_path, 
-        - 2nd till n-th entries: key = file type, value = count  
-    '''
+    Output: n-items Dict
+        - 1st entry being folder_path,
+        - 2nd till n-th entries: key = file type, value = count
+    """
     file_type_list_raw = glob.glob(os.path.join(folder_path, "*.*"))
-    file_type_list = [x.split(".")[1] for x in [x.split("\\")[-1] for x in file_type_list_raw]]
+    file_type_list = [
+        x.split(".")[1]
+        for x in [x.split("\\")[-1] for x in file_type_list_raw]
+    ]
     file_type_counter = defaultdict(list)
     file_type_counter["dir"] = folder_path
     for file_type in file_type_list:
@@ -51,14 +59,16 @@ def count_files(folder_path: str) -> dict:
     return file_type_counter
 
 
-
 def fileParser(raw_path: str):
     """
     Returns a list of line-by-line breakdown of file content, with comments removed.
     """
     comment_indicators = ["//"]
-    exclude_indicators = ["Color", "PATH_RES"] #exclude lines whose values always get updated along with files
-    with open(raw_path, "r") as file:
+    exclude_indicators = [
+        "Color",
+        "PATH_RES",
+    ]  # exclude lines whose values always get updated along with files
+    with open(raw_path) as file:
         content = file.read()
         content_lines = content.split(";")
         temp = []
@@ -69,25 +79,25 @@ def fileParser(raw_path: str):
                 delete_target += re.findall(f"{ind}.*\n*", line)
             for targ in delete_target:
                 line = line.replace(targ, "")
-            line = line.replace("\n","")
+            line = line.replace("\n", "")
             exclude_flags = [x for x in exclude_indicators if x in line]
             if exclude_flags == []:
                 temp.append(line)
     return temp
 
+    # TODO: parse result into things
+    # for line in temp:
+    #     cleaned = line.replace("\n","").replace(" ","").replace("\t","")
+    #     if (cleaned.find("[")!=-1):
+    #         # print(cleaned.find("["))
+    #         # print(cleaned)
+    #         split_index = cleaned.find("[")
+    #         a = [cleaned[:split_index], cleaned[split_index+1 : -2]]
+    #     else:
+    #         a = cleaned.split("=")
 
-        # TODO: parse result into things
-        # for line in temp:
-        #     cleaned = line.replace("\n","").replace(" ","").replace("\t","")
-        #     if (cleaned.find("[")!=-1):
-        #         # print(cleaned.find("["))
-        #         # print(cleaned)
-        #         split_index = cleaned.find("[")
-        #         a = [cleaned[:split_index], cleaned[split_index+1 : -2]]
-        #     else:
-        #         a = cleaned.split("=")
-            
-        #     result[a[0]] = a[1]
+    #     result[a[0]] = a[1]
+
 
 def fileFilter(file_list: list, target_types: list) -> list:
     get_type = lambda x: x.split("\\")[-1].split(".")[-1]
