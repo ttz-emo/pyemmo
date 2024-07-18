bf6a740c510aef7130b03dbff0facbf2643377f7
Max Schuler <max.schuler@thws.de>
2024-07-18 11:24:50 +0200

    [CC] added ipynb of runSim Script and gen script by nbconvert --to script [CC] removed hardcoded paths in runSim [CC] added bar resistance plot info

--- workingDirectory/test_asm/runSim_BlockedRotor.py
+++ workingDirectory/test_asm/runSim_BlockedRotor.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 # %%
 # %matplotlib widget
 import os
@@ -15,6 +21,7 @@ from pyemmo.functions.import_results import (
     plot_timetable_dat,
     read_RegionValue_dat,
 )
+from pyemmo.definitions import ROOT_DIR
 from definitions import MODEL_NAME, MODEL_DIR
 
 
@@ -41,14 +48,17 @@ if add_StreamHandler:
 
 logger = logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
-# %%
+
+# In[2]:
+
+
 logging.debug(f"Start simulation at {time.ctime()}")
 start = time.perf_counter()
 
 nbr_bars = 9
 
 f_r = 50
-I_eff = 20
+I_eff = 50
 n = 0
 f_s = f_r + 2 * n / 60
 T_s = 1 / f_s
@@ -71,6 +81,11 @@ logging.info(
 logging.debug("Timestep %e s.", timestep)
 logging.debug("One time step equals %f° mechanical degrees.", winkelschritt)
 logging.debug("Stop time of simulation: %.7e s", int(nbr_timesteps) * timestep)
+
+
+# In[3]:
+
+
 # %%
 resId = f"blockedRotor_{f_r}Hz_{nbr_stator_periods}Periods_{nbr_steps_per_period}Steps_R_dyn2_thers_{thers}A"
 paramDict = {
@@ -130,6 +145,11 @@ logging.info(
 )
 if sim_duration > nbr_timesteps:
     results["sim_duration"] = str(datetime.timedelta(seconds=sim_duration))
+
+
+# In[4]:
+
+
 # %%
 out_dict = paramDict.copy()
 out_dict.update(results)
@@ -139,6 +159,11 @@ if not os.path.isfile(json_res_path):
     # TODO: Move this to runCalcForCurrent function
     with open(json_res_path, "w", encoding="utf-8") as jFile:
         json.dump(out_dict, jFile, indent=4, cls=NumpyEncoder)
+
+
+# In[5]:
+
+
 # %%
 # plot currents
 nbr_timesteps = len(
@@ -151,6 +176,11 @@ ax.plot(results["time"], results["current"]["c"], label="w")
 ax.legend()
 ax.grid(True)
 ax.minorticks_on()
+
+
+
+# In[6]:
+
 
 # %%
 # PLOT STABSTRÖME
@@ -168,7 +198,7 @@ for nBar in range(nbr_bars):
 axi.set_ylabel("Strom in A")
 if t[-1] > 2 * T_s:  # if specific amount of periods is calculated
     axi.set_xlim(
-        t[int(t.size * ((nbr_stator_periods - 2) / nbr_stator_periods))], t[-1]
+        t[int(t.size * ((nbr_stator_periods - 1) / nbr_stator_periods))], t[-1]
     )
 axi.legend()
 axi.grid(True)
@@ -197,28 +227,44 @@ if os.path.isfile(resfile):
     ax.set_title(f"Stab {nBar+1}")
     ax.set_xlim(T_s * (nbr_stator_periods - 1), T_s * nbr_stator_periods)
     # %%
+    # Plot: R = U/I (Stab)
     fig, ax = plt.subplots()
     ax: Axes = ax
     r_bar = np.mean(U_bars[1:, nBar] / I_bars[1:, nBar])
+    UdR = U_bars[:, nBar] / I_bars[:, nBar]
     line_u = ax.plot(
-        t, U_bars[:, nBar] / I_bars[:, nBar], label="Widerstand", color="b"
+        t,
+        UdR,
+        label=f"Widerstand Stab {nBar}",
+        color="b",
     )
-    ax.set_xlim(
-        t[int(t.size * (nbr_stator_periods - 2) / nbr_stator_periods)], t[-1]
+    i_last_period = int(t.size * (nbr_stator_periods - 2) / nbr_stator_periods)
+    ax.set_xlim(t[i_last_period], t[-1])
+    ax.set_xlabel("time in s")
+    ax.set_ylabel("Resistance in $\\Omega$")
+    ax.set_ylim(
+        np.min(UdR[i_last_period:-1]),
+        np.max(UdR[i_last_period:-1]),
     )
-    ax.set_ylim(-1e-5, 1e-5)
     ax.grid(True)
+    ax.legend()
 
     # %
     fig, ax = plt.subplots()
     ax: Axes = ax
     for nBar in range(nbr_bars):
         line_u = ax.plot(t, U_bars[:, nBar], label=f"Stab {nBar}")
+    ax.set_xlabel("time in s")
     ax.set_ylabel("Spannung in V")
     # ax.set_xlim(14.75,15)
     ax.legend()
     # ax.set_ylim(-1e-5,1e-5)
     ax.grid(True)
+
+
+
+# In[7]:
+
 
 # %%
 # Induzierte Spannung Stator
@@ -241,6 +287,11 @@ ax.legend(loc=1)
 t = results["time"]
 # ax.set_xlim(t[int(t.size * (nbr_stator_periods - 2) / nbr_stator_periods)], t[-1])
 ax.grid(True)
+
+
+# In[8]:
+
+
 # %%
 # Plot Drehmoment
 fig, ax = plt.subplots()
@@ -292,7 +343,13 @@ print(
 )
 # read data from static resistance calculation
 t_stat, M_stat = read_timetable_dat(
-    r"C:\Users\ganser\AppData\Local\Programs\pyemmo\workingDirectory\test_asm\res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz\blockedRotor_50Hz_80Periods_64Steps_R_stat\Ts.dat"
+    os.path.join(
+        ROOT_DIR,
+        r"workingDirectory\test_asm",
+        "res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz",
+        "blockedRotor_50Hz_80Periods_64Steps_R_stat",
+        "Ts.dat",
+    )
 )
 M_mean_stat = np.mean(M_stat[int(t_stat.size * 79 / 80) :])
 print(
@@ -301,8 +358,13 @@ print(
 
 # ax.plot(t_stat, M_stat, ".-", label="statischer Widerstand")
 # ax.legend()
+
+
+# In[9]:
+
+
 # %%
-# PLOT RESISTANCES
+# PLOT internal GetDP resistances
 resfile = os.path.join(sim_res_dir, "R_bar_1.dat")
 if os.path.isfile(resfile):
     R_bar_dc = 3.572345668067562e-05
@@ -353,6 +415,11 @@ if os.path.isfile(resfile):
     t, R_bar = read_timetable_dat(resfile)
     ax.plot(t, R_bar, label=f"R (Circuit)", marker=".")
     ax.legend()
+
+
+# In[10]:
+
+
 # %%
 fig, ax = plt.subplots()
 ax: Axes = ax
@@ -404,8 +471,18 @@ if not os.path.isfile(f_name_mxwl_export):
         identifier=["Time (s)", "Torque_Onelab (Nm)"],
         filepath=f_name_mxwl_export,
     )
+
+
+# In[11]:
+
+
 # %%
 logging.shutdown()
+
+
+# In[12]:
+
+
 # %%
 # PLOT STABSTROM, -SPANNUNG UND -WIDERSTAND
 # Plot U_bar
@@ -442,8 +519,28 @@ line_r = axr.plot(t, R_bar_rt, label=f"Stabwiderstand", marker=".", color="r")
 # Add single legend
 ax.legend(handles=[line_u[0], line_i[0], line_r[0]], loc=2)
 
+
+
+# In[16]:
+
+
 # %%
 # Compare torque
+def import_mean_torque(res_dir: str):
+    resfile = os.path.join(
+        res_dir,
+        "Ts.dat",
+    )
+    _, torque_s = read_timetable_dat(resfile)
+    resfile = os.path.join(
+        res_dir,
+        "Tr.dat",
+    )
+    t, torque_r = read_timetable_dat(resfile)
+    torque_mean = np.mean([torque_s, torque_r], axis=0)
+    return t, torque_mean
+
+
 fig, ax = plt.subplots()
 ax: Axes = ax
 nBar = 0
@@ -455,23 +552,47 @@ line_i = ax.plot(
     results["time"],
     results["torque"]["mean"],
     marker=".",
-    markersize = 4,
-    mfc = (1,1,1),
+    markersize=4,
+    mfc=(1, 1, 1),
     label="Reset Previous Value",
 )
-resfile = r"C:\Users\ganser\AppData\Local\Programs\pyemmo\workingDirectory\test_asm\res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz\blockedRotor_50Hz_80Periods_128Steps_R_dyn_thers_100A\Ts.dat"
-t, torque_s = read_timetable_dat(resfile)
-resfile = r"C:\Users\ganser\AppData\Local\Programs\pyemmo\workingDirectory\test_asm\res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz\blockedRotor_50Hz_80Periods_128Steps_R_dyn_thers_100A\Tr.dat"
-t, torque_r = read_timetable_dat(resfile)
-torque_mean = np.mean([torque_s, torque_r], axis=0)
+
+## Plot Torque when resetting to DC Resistance
+t, torque_mean = import_mean_torque(
+    os.path.join(
+        ROOT_DIR,
+        r"workingDirectory\test_asm",
+        "res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz",
+        "blockedRotor_50Hz_80Periods_128Steps_R_dyn_thers_100A",
+    )
+)
 line_i = ax.plot(
     t,
     torque_mean,
     marker=".",
-    markersize = 4,
-    markevery = 1,
-    mfc = (1,1,1),
+    markersize=4,
+    markevery=1,
+    mfc=(1, 1, 1),
     label="Reset DC",
+    alpha=1,
+)
+## Plot Torque without resetting of resistance
+t, torque_mean = import_mean_torque(
+    os.path.join(
+        ROOT_DIR,
+        r"workingDirectory\test_asm",
+        "res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz",
+        "blockedRotor_50Hz_80Periods_64Steps_R_stat",
+    )
+)
+line_i = ax.plot(
+    t,
+    torque_mean,
+    marker=".",
+    markersize=4,
+    markevery=1,
+    mfc=(1, 1, 1),
+    label="No Reset",
     alpha=1,
 )
 ax.legend()
@@ -479,9 +600,15 @@ ax.set_xlabel("time in s")
 ax.set_ylabel("torque in Nm")
 ax.set_ylim(-40, 40)
 print(f"Mean Reset DC = {np.mean(torque_mean[int(t.size * 79 / 80) :])}")
-print(f"""Mean Reset Previous = {np.mean(results["torque"]["mean"][int(results["time"].size * 79 / 80) :])}""")
+print(
+    f"""Mean Reset Previous = {np.mean(results["torque"]["mean"][int(results["time"].size * 79 / 80) :])}"""
+)
 
-#%%
+
+# In[14]:
+
+
+# %%
 # Compare currents
 fig, ax = plt.subplots()
 ax: Axes = ax
@@ -498,7 +625,13 @@ line_i = ax.plot(
     label="Reset Previous Value",
 )
 
-resfile = r"C:\Users\ganser\AppData\Local\Programs\pyemmo\workingDirectory\test_asm\res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz\blockedRotor_50Hz_80Periods_128Steps_R_dyn_thers_100A\I_bar_1.dat"
+resfile = os.path.join(
+    ROOT_DIR,
+    r"workingDirectory\test_asm",
+    "res_Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz",
+    "blockedRotor_50Hz_80Periods_128Steps_R_dyn_thers_100A",
+    "I_bar_1.dat",
+)
 t, I_bar_dyn1 = read_timetable_dat(resfile)
 line_i = ax.plot(
     t,
@@ -510,4 +643,3 @@ ax.legend()
 ax.set_xlabel("time in s")
 ax.set_ylabel("current in A")
 
-#%%
