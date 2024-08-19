78f8c731e360b63f5e934b965af4c081f8ee87e5
Max Schuler <max.schuler@thws.de>
2024-08-19 11:35:31 +0200

    [CC] updated half model files

--- workingDirectory/test_asm/HalfModel/definitions/__init__.py
+++ workingDirectory/test_asm/HalfModel/definitions/__init__.py
@@ -3,8 +3,10 @@ import os
 from pyemmo.definitions import ROOT_DIR
 
 # Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz
-MODEL_NAME = "Test_1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz"
+MODEL_NAME = "1PH8135_1_D0_W92_P14k4W_ohneRotNutSchlitz"
 MODEL_DIR = os.path.abspath(os.path.dirname(__file__) + r"\..")
 print(MODEL_DIR)
-MODEL_JSON_FILE = os.path.join(MODEL_DIR, MODEL_NAME + ".json")
+MODEL_JSON_FILE = os.path.abspath(
+    os.path.join(MODEL_DIR + r"\..", MODEL_NAME + ".json")
+)
 PARAM_FILE = os.path.join(MODEL_DIR, "simuInfo.json")
