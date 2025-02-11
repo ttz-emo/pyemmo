3bc214aa576a6c4d254ef89633f523c99b0a1e92
Max Schuler <max.schuler@thws.de>
2025-02-11 12:49:20 +0100

    [WP] finished first working example of cutOut code for partly overlaping intersections (radially)

--- pyemmo/api/json/SurfaceJSON.py
+++ pyemmo/api/json/SurfaceJSON.py
@@ -20,12 +20,15 @@
 """Module for json api surface class"""
 from __future__ import annotations
 
+import gmsh
 from numpy import pi
 
 from ...script.geometry.circleArc import CircleArc
 from ...script.geometry.line import Line
 from ...script.geometry.spline import Spline
 from ...script.geometry.surface import Surface
+from ...script.gmsh import DimTag
+from ...script.gmsh.gmsh_surface import GmshSurface
 from ...script.material.material import Material
 from . import globalCenterPoint
 
@@ -269,5 +272,128 @@ class SurfaceAPI(Surface):
             return dup_surf
         # if the angle is zero: give back the old surface
         return self
+
+    def cutOut(self, ToolSurface: SurfaceAPI, keepTool: bool = True) -> None:
+        """Cut out a Tool Surface from the Parent surface by Boolean Difference"""
+        self._cut.append(ToolSurface)
+        ToolSurface.setTool()  # set to the tool surface
+        if not keepTool:
+            ToolSurface.delete = True
+        cut_dim_tags: list[DimTag] = [(2, ToolSurface.id)]
+        # cut out tools of tools aswell!
+        if ToolSurface.tools:
+            raise RuntimeError("Tool surfaces can not have tools for now!")
+            # TODO: Implement cutting out tools of tools, maybe be creating new united
+            # surface from all tools.
+            tools = ToolSurface.tools
+            while tools:
+                # while tools array is not empty
+                new_tools = []
+                for tool in tools:
+                    # add tool surface to cut out
+                    cut_dim_tags.append((2, tool.id))
+                    if tool.tools:
+                        # if tool itself has tools
+                        new_tools.extend(tool.tools)  # update new_tools list
+                tools = new_tools  # reset tools
+        # fragment resutls in a list of new surfaces tags + a map that tells which of
+        # the new surfaces together form the old surface. So for e.g. if surface 2,
+        # which intersects surface 1 partly, is subtracted from surface 1, then 3 new
+        # surfaces are created:
+        # Tag 1: the part of surface 1 that is not intersected by surface 2
+        # Tag 2: the part of surface 1 that is intersected by surface 2
+        # Tag 3: the part of surface 2 that is not intersected by surface 1
+        # And outDimTagsMap will tell you which of the new surfaces build the original
+        # surface, like:
+        #   [[(2,1),(2,2)],[(2,2),(2,3)]]
+        # This means e.g. that the original surface with old tag 1 is build by the new
+        # surfaces 1 and 2.
+        out_dim_tags, out_dim_tags_map = gmsh.model.occ.fragment(
+            [(2, self._id)], cut_dim_tags, removeTool=ToolSurface.delete
+        )
+        if (
+            out_dim_tags_map[1] == cut_dim_tags
+            and out_dim_tags_map[0] == out_dim_tags + cut_dim_tags
+        ):
+            # tool surface did not change and parent surface is fromed by
+            # new surface + tool
+            self.id = out_dim_tags[0][1]  # set id to new surface tag
+            return None
+        if out_dim_tags_map[0] == out_dim_tags and out_dim_tags_map[1] == [
+            out_dim_tags[1]
+        ]:
+            # same as first test but tool surface was copied with new tag
+            self.id = out_dim_tags[0][1]  # set id to new surface tag
+            ToolSurface.id = out_dim_tags[1][1]  # set id of tool surfacee to new tag
+            return None
+        if len(out_dim_tags) > (len(cut_dim_tags) + 1):
+            # extra surface(s) was/were created -> shift new surface by self.angle and
+            # subtract from self:
+            self.id = out_dim_tags[0][1]  # TODO: Check if this is the right surface
+            gmsh.model.setEntityName(2, self.id, self.name)  # update name of surface
+
+            new_tool_dimtags = out_dim_tags_map[-1]
+            for dimtag in new_tool_dimtags:
+                if dimtag not in out_dim_tags_map[0]:
+                    # new (outer) tool surface since it is not part of the original
+                    # surface
+                    # rotate it, cut it out and add it to the tools list
+                    gmsh.model.occ.rotate(
+                        [dimtag],
+                        globalCenterPoint.x,
+                        globalCenterPoint.y,
+                        globalCenterPoint.z,
+                        0,
+                        0,
+                        1,
+                        self.angle,
+                    )
+                    # There is no way to initialize SurfaceAPI with a GmshSurface object
+                    # directly so we need to create a GmshSurface first and extract the
+                    # curve loop from it:
+                    gmsh.model.occ.synchronize()
+                    gmsh_surf = GmshSurface(tag=dimtag[1])
+                    # somehow the rotation does not work here. Do rotation manually
+                    # before creating the GmshSurface
+                    # gmsh_surf.rotateZ(globalCenterPoint, self.angle)
+                    # gmsh.model.occ.synchronize()
+
+                    new_tool = SurfaceAPI(
+                        name=ToolSurface.name + "_1",
+                        idExt=ToolSurface.idExt,
+                        curves=gmsh_surf.curve,
+                        material=ToolSurface.material,
+                        nbrSegments=ToolSurface.NbrSegments,
+                        angle=ToolSurface.angle,
+                        meshSize=ToolSurface.meshSize,
+                    )
+                    # remove gmsh_surf since the new_tool has new id but is same surface
+                    gmsh.model.occ.remove([dimtag])  # remove gmsh_surf
+                    self.cutOut(new_tool, keepTool=keepTool)
+                    # self.tools.append(new_tool)
+                else:
+                    # replace old tool surface with new (not rotated) tool surface
+                    gmsh_surf = GmshSurface(tag=dimtag[1])
+                    # There is no way to initialize SurfaceAPI with a GmshSurface object
+                    # directly so we need to create a GmshSurface first and extract the
+                    # curve loop from it
+                    replace_tool = SurfaceAPI(
+                        name=ToolSurface.name + "_0",
+                        idExt=ToolSurface.idExt,
+                        curves=gmsh_surf.curve,
+                        material=ToolSurface.material,
+                        nbrSegments=ToolSurface.NbrSegments,
+                        angle=ToolSurface.angle,
+                        meshSize=ToolSurface.meshSize,
+                    )
+                    # remove gmsh_surf since the new_tool has new id but is same surface
+                    gmsh.model.occ.remove([dimtag])
+                    self.tools[self.tools.index(ToolSurface)] = replace_tool
+            # end of for loop remove original tool surface
+            gmsh.model.occ.remove([(2, ToolSurface.id)])
+        else:
+            raise ValueError(
+                "Number of output surfaces is less than number of input surfaces!"
+            )
 
     # TODO: Update plot function to also plot tool surfaces
