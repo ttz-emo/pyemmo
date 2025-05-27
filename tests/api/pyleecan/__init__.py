9e866e1b87d0867cf882ef0143611ac161809de7
Max Schuler <max.schuler@thws.de>
2025-05-27 14:56:33 +0200

    [NF] added pyleecan machine fixtures for api tests

--- tests/api/pyleecan/__init__.py
+++ tests/api/pyleecan/__init__.py
@@ -18,6 +18,119 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 from os.path import join
+
+import pytest
+
+# pylint: disable=locally-disabled, no-name-in-module
+from pyleecan.Functions.load import load
+
 from tests.api import TEST_API_DATA_DIR
 
 TEST_API_PYLCN_DATA_DIR = join(TEST_API_DATA_DIR, "pyleecan")
+
+
+@pytest.fixture(scope="function")
+def Toyota_Prius():
+    """Fixture to load machine object for testing
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW11
+    Rotor Type: LamHole
+    Hole Type: HoleM50
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "00_prius_machine.json"))
+
+
+@pytest.fixture(scope="function")
+def Toyota_Prius_2():
+    """Fixture to load machine object for testing (old pyleecan version 1.3.8)
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW11
+    Rotor Type: LamHole
+    Hole Type: HoleM50
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "00_prius_machine.json"))
+
+
+@pytest.fixture(scope="function")
+def SIPMSM_BA():
+    """Fixture to load machine object for testing.
+    Machine from bachelor thesis of Jan Knoll with 10 Poles, 12 Slots and 6 Axial
+    Cooling Ducts on the stator. This results in a different symmetry on the stator than
+    the number of slots.
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW22
+    Rotor Type: LamSlotMag
+    Hole Type: SlotM11
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "02_spmsm_muster_02.json"))
+
+
+@pytest.fixture(scope="function")
+def SYNRM_ZAW():
+    """Fixture to load machine object for testing
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW28
+    Rotor Type: LamHole
+    Hole Type: HoleM54
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "03_synrm_muster_Bachelor_zaw.json"))
+
+
+@pytest.fixture(scope="function")
+def SYNRM_ZWW():
+    """Fixture to load machine object for testing
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW28
+    Rotor Type: LamHole
+    Hole Type: HoleM54
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "03_synrm_muster_Bachelor_zaw.json"))
+
+
+@pytest.fixture(scope="function")
+def IPMSM():
+    """Fixture to load machine object for testing
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW11
+    Rotor Type: LamHole
+    Hole Type: HoleM53
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "IPMSM_B.json"))
+
+
+@pytest.fixture(scope="function")
+def SIPMSM_1():
+    """Fixture to load machine object for testing (Pyleecan version 1.2.1)
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW22
+    Rotor Type: LamSlotMag
+    Hole Type: SlotM11
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "SPMSM_002.json"))
+
+
+@pytest.fixture(scope="function")
+def SIPMSM_2():
+    """Fixture to load machine object for testing (Pyleecan version 1.2.1)
+
+    Stator Type: LamSlotWind
+    Slot Type: SlotW22
+    Rotor Type: LamSlotMag
+    Hole Type: SlotM11
+    """
+    # Create a LamSlot object with some example parameters
+    return load(join(TEST_API_PYLCN_DATA_DIR, "SPMSM_003.json"))
