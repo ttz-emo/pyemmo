2d5ebfc99be3b44e683dbce5794b4f0278af4fba
Max Schuler <max.schuler@thws.de>
2025-06-27 16:47:53 +0200

    [NF] added tests for load Material from database

---
+++ tests/script/material/test_material_database.py
@@ -0,0 +1,44 @@
+#
+# Copyright (c) 2018-2024 M. Schuler, TTZ-EMO,
+# Technical University of Applied Sciences Wuerzburg-Schweinfurt.
+#
+# This file is part of PyEMMO
+# (see https://gitlab.ttz-emo.thws.de/ag-em/pyemmo).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Module to test all materials can be loaded from the material database."""
+import pytest
+
+from pyemmo.script.material.material import Material
+
+
+@pytest.mark.parametrize("material_file_name", ["air", "aluminum"])
+def test_linear_material_load(material_file_name):
+    """Test that all materials can be loaded from the material database."""
+    # Load the material database
+    mat = Material.load(material_file_name)
+    assert mat.linear is True
+
+
+@pytest.mark.parametrize(
+    "material_file_name", ["steel_1008", "steel_1010", "M250_35A_0050Hz"]
+)
+def test_nonlinear_material_load(material_file_name):
+    """Test that all materials can be loaded from the material database."""
+    # Load the material database
+    mat = Material.load(material_file_name)
+    assert mat.linear is False
+    assert len(mat.BH) != 0
