7098b4fa315891279fa47809bf8ab81f660a7d86
Max Schuler <max.schuler@thws.de>
2025-11-19 13:09:00 +0100

    [WP] fix test for pyleecan api test_create_gmsh_surface

--- tests/api/pyleecan/test_create_surface.py
+++ tests/api/pyleecan/test_create_surface.py
@@ -32,6 +32,7 @@ from pyleecan.Functions.labels import HOLEM_LAB, HOLEV_LAB, get_obj_from_label
 
 from pyemmo.api.machine_segment_surface import MachineSegmentSurface
 from pyemmo.api.pyleecan import PyleecanMachine
+from pyemmo.api.pyleecan.build_pyemmo_material import build_pyemmo_material
 from pyemmo.api.pyleecan.create_gmsh_surf import create_gmsh_surface
 
 from . import Toyota_Prius  # pylint: disable=W0611
@@ -44,7 +45,13 @@ def test_create_gmsh_surface(machine, request):
     machine: PyleecanMachine = request.getfixturevalue(machine)
     sym, _ = machine.rotor.comp_periodicity_geo()
     rotor_surfs = machine.rotor.build_geometry(sym=sym, alpha=0)
-    lam_surf = create_gmsh_surface(rotor_surfs.pop(0))
+    surf = rotor_surfs.pop(0)
+    lam_surf = create_gmsh_surface(
+        surf,
+        nbr_segments=sym,
+        material=build_pyemmo_material(machine.rotor.mat_type),
+        name=surf.label,
+    )
     geometry_list.append(lam_surf)
     for surf in rotor_surfs:
         # get pyleecan object for material identification
@@ -64,11 +71,17 @@ def test_create_gmsh_surface(machine, request):
             raise e
 
         # translating the surface
-        pyemmo_surf = create_gmsh_surface(surf)
+        pyemmo_surf = create_gmsh_surface(
+            surf, sym, build_pyemmo_material(material), surf.label
+        )
         pyemmo_surf.plot()
         if any(label in surf.label for label in (HOLEM_LAB, HOLEV_LAB)):
             lam_surf.cutOut(pyemmo_surf)
         else:
             geometry_list.append(pyemmo_surf)
-    plot(geometry_list)
-    assert len(geometry_list) == 8
+    # there should only be the lamination surface in the geo list
+    assert len(geometry_list) == 1
+    # there should be 5 surfaces subtracted from this lam surface
+    assert len(lam_surf.tools) == 5
+    # there should be 6 surfaces created in gmsh
+    assert len(gmsh.model.getEntities(2)) == 6
