40fbd9bcc4d1628efa7724212b70af695c21d24b
Max Schuler <max.schuler@thws.de>
2026-02-12 16:19:11 +0100

    [WP] added UML diagram creation via `pyreverse` (tool in `pylint`)

---
+++ doc/uml_diagrams.py
@@ -0,0 +1,64 @@
+"""
+This module implements a script to create UML diagrams of PyEMMO class structure for
+the documentation.
+"""
+
+from __future__ import annotations
+
+import subprocess
+from os import makedirs
+from os.path import isdir, join
+
+from pyemmo.definitions import MAIN_DIR
+
+script_dir = join(MAIN_DIR, "script")
+uml_out_dir = join(MAIN_DIR, "..", "doc", "images", "uml")
+if not isdir(uml_out_dir):
+    makedirs(uml_out_dir)
+
+
+# # full script class and full package diagram.
+# subprocess.check_call(
+#     [
+#         "pyreverse",
+#         "-o",
+#         "png",
+#         "--output-directory",
+#         uml_out_dir,
+#         "--only-classnames",
+#         "-p",
+#         "PyEMMO",
+#         MAIN_DIR,
+#     ]
+# )
+
+# pyemmo subpackage class and module diagrams.
+for subpackage in ("script", "api", "functions"):
+    subprocess.check_call(
+        [
+            "pyreverse",
+            "-o",
+            "dot",
+            "--output-directory",
+            uml_out_dir,
+            "-p",
+            f"PyEMMO.{subpackage}",
+            join(MAIN_DIR, subpackage),
+        ]
+    )
+
+
+# script subpackage class and module diagrams.
+for subpackage in ("geometry", "gmsh", "material"):
+    subprocess.check_call(
+        [
+            "pyreverse",
+            "-o",
+            "dot",
+            "--output-directory",
+            uml_out_dir,
+            "-p",
+            f"PyEMMO.script.{subpackage}",
+            join(script_dir, subpackage),
+        ]
+    )
