127b5e2b96cbc3ec61c5887b4c1a0c9b3a6abd7f
Max Schuler <max.schuler@thws.de>
2026-03-16 15:50:12 +0100

    [CO] improve tutorial test by using subprocess `env` attribute to set getdp path + copy and modify tutorial script to not open GUI at test

--- tests/integrationTest/test_tutorial.py
+++ tests/integrationTest/test_tutorial.py
@@ -21,15 +21,17 @@
 from __future__ import annotations
 
 import logging
+import os
 import subprocess
 import sys
+from os import environ
 from os.path import abspath, dirname, join, normpath
 
 import pytest
 
 from pyemmo.definitions import ROOT_DIR
 
-from .. import GETDP_EXE
+from .. import GETDP_EXE, TEST_TEMP_DIR
 
 TUTORIAL_DIR = normpath(abspath(join(ROOT_DIR, "tutorials")))
 tutorial_files = ["pyleecan_api.py"]
@@ -43,15 +45,33 @@ if dirname(GETDP_EXE) not in sys.path:
 def test_run_tutorial_script(tutorial_file):
     """Test run tutorial files"""
     logger = logging.getLogger(__file__)
-    logger.info("Running tutorial file: %s", tutorial_file)
-    tuto_path = join(TUTORIAL_DIR, tutorial_file)
-    retcode = subprocess.call([sys.executable, tuto_path], shell=False)
-    assert retcode == 0, "Tutorial failed..."
 
-    # with open(tuto_path, encoding="utf-8") as tuto_file:
-    #     code = tuto_file.readlines()
-    #     # remove lines with GUI call to run tutorial without UI
-    #     _ = [code.remove(line) for line in code if "gmsh.fltk.run" in line]
-    #     # remove get_ipython
-    #     _ = [code.remove(line) for line in code if "get_ipython" in line]
-    #     exec("".join(code))
+    # Append GETDP_EXE path to the current PYTHONPATH if it exists
+    logger.debug("Adding GETDP_EXE path to subprocess env: %s", GETDP_EXE)
+    python_path = os.pathsep.join([environ.get("PYTHONPATH", ""), GETDP_EXE])
+    # Use a copy of the current environment so you are not affecting the parent
+    curr_env = environ.copy()
+    # updated environment to use in subprocess call
+    modified_env = curr_env | {"PYTHONPATH": python_path}
+
+    tuto_path = join(TUTORIAL_DIR, tutorial_file)
+    logger.debug("copy and modify tutorial script %s to not open GUI.", tuto_path)
+    with open(tuto_path, encoding="utf-8") as tuto_file:
+        code = tuto_file.readlines()
+        # remove lines with GUI call to run tutorial without UI
+        logger.debug("Removing 'gmsh.fltk.run' lines.")
+        _ = [code.remove(line) for line in code if "gmsh.fltk.run" in line]
+        # remove get_ipython
+        logger.debug("Removing 'get_ipython' lines.")
+        _ = [code.remove(line) for line in code if "get_ipython" in line]
+
+    tuto_path = join(TEST_TEMP_DIR, tutorial_file)
+    logger.debug("Copy remaining code to file %s", tuto_path)
+    with open(tuto_path, "w", encoding="utf-8") as tuto_file:
+        tuto_file.writelines(code)
+
+    logger.info("Running tutorial file: %s", tutorial_file)
+    retcode = subprocess.call(
+        [sys.executable, tuto_path], shell=False, env=modified_env
+    )
+    assert retcode == 0, "Tutorial failed..."
