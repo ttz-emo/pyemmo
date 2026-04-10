2f8268de7200929d70f91d7175b00d5c8841dc33
Max Schuler <max.schuler@thws.de>
2026-04-10 12:57:56 +0200

    [WP] added voltage source tutorial + update test script

--- workingDirectory/dev_test_stator_circuit.py
+++ workingDirectory/dev_test_stator_circuit.py
@@ -17,7 +17,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+# %%
 """Test stator voltage input"""
 from __future__ import annotations
 
@@ -26,22 +26,14 @@ import json
 import logging
 
 # %%
-import os
 import time
+from os.path import join
 
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 
-from pyemmo.definitions import TEST_DIR
-from pyemmo.functions.import_results import (
-    read_RegionValue_dat,
-    read_timetable_dat,
-)
-from pyemmo.functions.run_onelab import run_simulation
-
-MODEL_NAME = "Toyota_Prius.json"
-MODEL_DIR = os.path.join(TEST_DIR, "data", "api", "pyleecan")
+from pyemmo.functions.run_onelab import find_getdp, run_simulation
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -67,6 +59,36 @@ if add_StreamHandler:
 
 logger = logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
+# %% Create model
+from pyleecan.definitions import DATA_DIR
+from pyleecan.Functions.load import load  # Pylint: ignore=no-name-in-module
+
+from pyemmo.api.pyleecan import main as pyleecan_api
+from pyemmo.definitions import RESULT_DIR
+
+MODEL_NAME = "Toyota_Prius"
+IPMSM_A = load(join(DATA_DIR, "Machine", f"{MODEL_NAME}.json"))
+
+# you can set the output verbosity of the model creation using the pyemmo logger.
+# The logging module by default implements DEBUG, INFO, WARNING, ERROR, FATAL log
+# levels, which are simply constants for integer levels, e.g. logging.DEBUG == 10
+# You can use evel(logging.DEBUG -1) to trigger extended debugging, which shows
+# additional information in plots and opens the Gmsh GUI at different levels in the
+# model creation.
+logging.getLogger("pyemmo").setLevel(logging.INFO)
+
+# Run the main function of the pyleecan api:
+pyemmo_script = pyleecan_api.main(
+    pyleecan_machine=IPMSM_A,
+    model_dir=join(RESULT_DIR, "Toyota_Prius_dev_stator_circuit"),
+    use_gui=False,  # select if you want to open the final model in Gmsh.
+    gmsh="",  # optional gmsh executable.
+    # If use_gui is True, pyemmo will try to find a executable on your computer.
+    getdp="",  # optional getdp executable. For simulation in the GUI.
+)
+
+MODEL_DIR = pyemmo_script.script_path
+
 # %%
 logging.debug(f"Start simulation at {time.ctime()}")
 start = time.perf_counter()
@@ -77,14 +99,14 @@ n = 750
 f_s = p * n / 60
 T_s = 1 / f_s
 nbr_stator_periods = 3
-nbr_sect = 2048 / 2  # Bandsegmentierung
+nbr_sect = 2048 / 2  # Band object segmentation
 multi = 4  # Default=4 number of Segments per timestep
 timestep = T_s / 16
-winkelschritt = n / 60 * 360 * timestep  # Default: 0.703125
+angular_step = n / 60 * 360 * timestep  # Default: 0.703125
 nbr_steps = T_s / timestep * nbr_stator_periods
 logging.info("Simulation should execute %i time steps.", int(nbr_steps) + 1)
 logging.debug("Timestep %e s.", timestep)
-logging.debug("One time step equals %f° mechanical degrees.", winkelschritt)
+logging.debug("One time step equals %f° mechanical degrees.", angular_step)
 logging.debug("Stop time of simulation: %.7e s", int(nbr_steps) * timestep)
 # %%
 resId = f"{U_eff}V_{n}rpm_{nbr_stator_periods}Periods"
@@ -93,8 +115,8 @@ paramDict = {
         "RPM": float(n),
         "nbrStatorPeriods": nbr_stator_periods,
         "initrotor_pos": 0.0,
-        "d_theta": winkelschritt,
-        "finalrotor_pos": winkelschritt * nbr_steps,
+        "d_theta": angular_step,
+        "finalrotor_pos": angular_step * nbr_steps,
         "Flag_SrcType_Stator": 2,  # 1:current source, 2:voltage source, 0:CEMF (not available yet)
         "CircuitConnection": 0,  # 0:Star, 1:Delta
         "VV": U_eff * np.sqrt(2),  # voltage amplitude
@@ -112,13 +134,13 @@ paramDict = {
         # "L_endring_segment": 2e-9 / 2,
         # "Flag_Cir_RotorCage": 0,
         # "Flag_Calculate_VW": 1,
-        "msh": os.path.join(MODEL_DIR, "default.msh"),
+        # "msh": os.path.join(MODEL_DIR, "default.msh"),
         # "Flag_SecondOrder": 0,
-        "exe": r"getdp.exe",
-        "res": os.path.join(MODEL_DIR, f"res_{MODEL_NAME}"),
+        "exe": find_getdp(),
+        "res": pyemmo_script.results_path,
     },
-    "pro": os.path.join(MODEL_DIR, MODEL_NAME + ".pro"),
-    "gmsh": {"exe": "gmsh.exe"},
+    "pro": pyemmo_script.pro_file_path,
+    "gmsh": {"exe": ""},
     # TODO: Extend gmsh element to dict with parameters, like:
     # "gmsh": {
     #     "exe": "gmsh.exe",
@@ -161,33 +183,16 @@ if sim_duration > 1:
 # -setnumber NbrParallelPaths 1
 # -setstring ResPath D:\pyemmo\Results\pyleecanAPI\prius_debug\res_IPMSM_Muster_1
 
-# %%
-out_dict = paramDict.copy()
-out_dict.update(results)
-with open(
-    os.path.join(paramDict["res"], resId, f"{resId}.json"),
-    "w",
-    encoding="utf-8",
-) as jFile:
-    json.dump(out_dict, jFile, indent=4, cls=NumpyEncoder)
+# # %%
+# out_dict = paramDict.copy()
+# out_dict.update(results)
+# with open(
+#     os.path.join(paramDict["res"], resId, f"{resId}.json"),
+#     "w",
+#     encoding="utf-8",
+# ) as jFile:
+#     json.dump(out_dict, jFile, indent=4, cls=NumpyEncoder)
 
-respath = os.path.join(paramDict["res"], resId)
-
-try:
-    if "voltage" not in results:
-        results["voltage"] = {}
-    for phase in "abc":
-        _, results["voltage"][phase] = read_timetable_dat(
-            os.path.join(respath, f"U{phase}.dat")
-        )
-        _, results["voltage"][phase + "_L"] = read_timetable_dat(
-            os.path.join(respath, f"U{phase}_w.dat")
-        )
-        _, results["current"][phase + "_L"] = read_timetable_dat(
-            os.path.join(respath, f"I{phase}_w.dat")
-        )
-except:
-    pass
 # %%
 # Show voltages in stator circuit
 fig, ax = plt.subplots()
@@ -195,8 +200,9 @@ ax: Axes = ax
 for phase in "abc":
     ax.plot(results["time"], results["voltage"][phase], label=f"Input Voltage {phase}")
 # extra loop to have results in order
-for phase in "abc":
-    ax.plot(results["time"], results["voltage"][phase + "_L"], label=f"U_L,{phase}")
+# for phase in "abc":
+#     ax.plot(results["time"], results["voltage"][phase ], label=f"U_L,{phase}")
+
 ax.set_ylabel("Spannung in V")
 ax.grid(True)
 ax.legend()
@@ -204,23 +210,21 @@ ax.set_xlim([results["time"][-1] - T_s, results["time"][-1]])
 
 # %%
 # show input and line currents
-try:
-    fig, ax = plt.subplots()
-    for phase in "abc":
-        ax.plot(results["time"], results["current"][phase], label=phase)
-    for phase in "abc":
+fig, ax = plt.subplots()
+for phase in "abc":
+    ax.plot(results["time"], results["current"][phase], label=phase)
+    if phase + "_w" in results["current"]:
         ax.plot(
             results["time"],
-            results["current"][phase + "_L"],
+            results["current"][phase + "_w"],
             label=phase + " line",
             linestyle="--",
         )
-    ax.set_ylabel("phase current in A")
-    ax.set_xlabel("time in s")
-    ax.grid(True)
-    ax.legend(loc=2)
-except Exception as e:
-    logging.error(e)
+ax.set_ylabel("phase current in A")
+ax.set_xlabel("time in s")
+ax.grid(True)
+ax.legend(loc=2)
+
 # %%
 # compare input voltage and input current
 fig, ax = plt.subplots()
@@ -235,35 +239,36 @@ for phase in "abc":
 for a in (ax, axi):
     a.set_xlim([results["time"][-1] - T_s, results["time"][-1]])
 ax.grid(True)
-ax.set_ylabel("Spannung in V")
-ax.legend(loc=1)
+ax.set_ylabel("Voltage in V")
+ax.legend(loc=2)
 
-axi.set_ylabel("Strom in A")
-axi.legend(loc=2)
+axi.set_ylabel("Current in A")
+axi.legend(loc=1)
 
 # %%
 # compare line voltage and line current
-fig, ax = plt.subplots()
-ax: Axes = ax
-axi: Axes = ax.twinx()
-for phase in "abc":
-    ax.plot(results["time"], results["voltage"][phase + "_L"], label=f"U_{phase},L")
-    # ax.plot(results["time"], results["voltage"][phase], label=f"U_L,{phase}")
+if "a_w" in results["current"]:
+    fig, ax = plt.subplots()
+    ax: Axes = ax
+    axi: Axes = ax.twinx()
+    for phase in "abc":
+        ax.plot(results["time"], results["voltage"][phase + "_w"], label=f"U_{phase},L")
+        # ax.plot(results["time"], results["voltage"][phase], label=f"U_L,{phase}")
 
-    axi.plot(
-        results["time"],
-        results["current"][phase + "_L"],
-        label=f"I_{phase},L",
-        linestyle="--",
-    )
-for a in (ax, axi):
-    a.set_xlim([results["time"][-1] - T_s, results["time"][-1]])
-ax.grid(True)
-ax.set_ylabel("Spannung in V")
-ax.legend(loc=1)
+        axi.plot(
+            results["time"],
+            results["current"][phase + "_w"],
+            label=f"I_{phase},L",
+            linestyle="--",
+        )
+    for a in (ax, axi):
+        a.set_xlim([results["time"][-1] - T_s, results["time"][-1]])
+    ax.grid(True)
+    ax.set_ylabel("Voltage in V")
+    ax.legend(loc=1)
 
-axi.set_ylabel("Strom in A")
-axi.legend(loc=2)
+    axi.set_ylabel("Current in A")
+    axi.legend(loc=2)
 
 # %%
 
@@ -271,15 +276,19 @@ axi.legend(loc=2)
 # Show induced voltage in stator phases
 fig, ax = plt.subplots()
 ax: Axes = ax
-# for phase in 'ABC':
-line_u = ax.plot(results["time"], results["inducedVoltage"]["A"], label="U_iA")
-line_u = ax.plot(results["time"], results["inducedVoltage"]["B"], label="U_iB")
-line_u = ax.plot(results["time"], results["inducedVoltage"]["C"], label="U_iC")
-# ax.set_ylabel("Spannung in V")
-# ax.set_xlim(14.5, 15)
-ax.legend()
-# ax.set_ylim(-1e-5,1e-5)
+for phase in results["inducedVoltage"].keys():
+    ax.plot(
+        results["time"][1:],
+        results["inducedVoltage"][phase],
+        label=f"$U_{{i,\\mathrm{{{phase}}}}}$",
+    )
+
+ax.set_ylabel("Voltage in V")
 ax.grid(True)
+ax.legend()
+ax.set_title("Induced Voltage")
+# ax.set_xlim(14.5, 15)
+# ax.set_ylim(-1e-5,1e-5)
 
 # %%
 try:
@@ -290,7 +299,7 @@ try:
     ax.plot(results["time"], results["flux"]["c"], label="c")
     ax.plot(results["time"], results["flux"]["d"], label="d")
     ax.plot(results["time"], results["flux"]["q"], label="q")
-    ax.set_ylabel("flux in Wb")
+    ax.set_ylabel("flux linkage in Wb")
     ax.set_xlabel("time in s")
     ax.grid(True)
     ax.legend()
@@ -298,24 +307,24 @@ except Exception as e:
     logging.error(e)
 
 
-# %%
-# Plot Drehmoment
-fig, ax = plt.subplots()
-ax: Axes = ax
-if os.path.isfile(os.path.join(respath, "Ts_vw.dat")):
-    t_t, ts_vw = read_RegionValue_dat(os.path.join(respath, "Ts_vw.dat"))
-    _, tr_vw = read_RegionValue_dat(os.path.join(respath, "Tr_vw.dat"))
-    ax.plot(t_t, np.mean([ts_vw, tr_vw], axis=0), label="VW")
+# # %%
+# # Plot Drehmoment
+# fig, ax = plt.subplots()
+# ax: Axes = ax
+# if os.path.isfile(os.path.join(respath, "Ts_vw.dat")):
+#     t_t, ts_vw = read_RegionValue_dat(os.path.join(respath, "Ts_vw.dat"))
+#     _, tr_vw = read_RegionValue_dat(os.path.join(respath, "Tr_vw.dat"))
+#     ax.plot(t_t, np.mean([ts_vw, tr_vw], axis=0), label="VW")
 
-ax.plot(
-    results["time"],
-    results["torque"],
-    ".-",
-    label="MST",
-)
-ax.legend(loc=1)
-ax.set_title("Drehmoment")
-ax.grid(True)
+# ax.plot(
+#     results["time"],
+#     results["torque"],
+#     ".-",
+#     label="MST",
+# )
+# ax.legend(loc=1)
+# ax.set_title("Drehmoment")
+# ax.grid(True)
 
 # %%
 logging.shutdown()
